2936e817c30bf7bafd2eb5285d8c8940fd70ae82
Paul Statezny <paulstatezny@gmail.com>
2018-02-14 15:08:16 -0700

    Add "create device" page

---
+++ priv/repo/migrations/20180214205620_devices_table_modifications.exs
@@ -0,0 +1,25 @@
+defmodule Beamware.Repo.Migrations.DevicesTableModifications do
+  use Ecto.Migration
+
+  def up do
+    alter table(:devices) do
+      add :description, :string
+      add :product, :string
+      add :platform, :string
+      modify :current_version, :string, null: true
+    end
+
+    create unique_index(:devices, [:tenant_id, :identifier])
+  end
+
+  def down do
+    drop unique_index(:devices, [:tenant_id, :identifier])
+
+    alter table(:devices) do
+      remove :description
+      remove :product
+      remove :platform
+      modify :current_version, :string, null: false
+    end
+  end
+end
