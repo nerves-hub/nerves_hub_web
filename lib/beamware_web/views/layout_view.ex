64618ee55f1560264c1208eab12f07e09b397f79
Paul Statezny <paulstatezny@gmail.com>
2018-02-15 16:48:05 -0700

    Move navigation to layout template

--- lib/beamware_web/views/layout_view.ex
+++ lib/beamware_web/views/layout_view.ex
@@ -1,3 +1,13 @@
 defmodule BeamwareWeb.LayoutView do
   use BeamwareWeb, :view
+
+  def navigation_links(conn) do
+    [
+      {"Manage Devices", device_path(conn, :index)},
+      {"Manage Firmware", firmware_path(conn, :index)},
+      {"Manage Updates", "#"},
+      {"Settings", account_path(conn, :edit)},
+      {"Tenant Settings", tenant_path(conn, :edit)}
+    ]
+  end
 end
