c00092fb94f6c2231f1b133b5dccce0781a98c36
Justin Schneck <jschneck@mac.com>
2018-05-22 09:42:16 -0400

    Do not start socket on client app start

--- client/lib/client.ex
+++ client/lib/client.ex
@@ -1,3 +1,10 @@
 defmodule BeamwareClient do
 
+  alias BeamwareClient.{Socket, DeviceChannel}
+
+  def connect do
+    {:ok, _socket} = Socket.start_link()
+    {:ok, _channel} = DeviceChannel.start_link(socket: BeamwareClient.Socket, topic: "device:lobby")
+    DeviceChannel.join()
+  end
 end
