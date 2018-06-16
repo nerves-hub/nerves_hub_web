4722ee9e680f5fa46a5b0b763c4e172497120058
Daniel Spofford <dannyspofford@gmail.com>
2018-06-16 13:42:42 -0500

    # This is a combination of 3 commits. # The first commit's message is: Split firmware metadata into distinct fields
    * Create migration and adjust schema to drop `timestamp` and `metadata`,
    and add `author`, `description`, `misc`, `uuid`, `vcs_identifier`
    * Adjust required and nullable fields
    * Split `Firmware.metadata_item/2` into `fetch_metadata_item/2` and
    `get_metadata_item/{2,3}` mirroring `Map.{fetch,get}` semantics
    * Update templates and views to respect changes
    
    # The 2nd commit message will be skipped:
    
    #	Split firmware metadata into distinct fields
    #
    #	* Create migration and adjust schema to drop `timestamp` and `metadata`,
    #	and add `author`, `description`, `misc`, `uuid`, `vcs_identifier`
    #	* Adjust required and nullable fields
    #	* Split `Firmware.metadata_item/2` into `fetch_metadata_item/2` and
    #	`get_metadata_item/{2,3}` mirroring `Map.{fetch,get}` semantics
    #	* Update templates and views to respect changes
    
    # The 3rd commit message will be skipped:
    
    #	Split firmware metadata into distinct fields
    #
    #	* Create migration and adjust schema to drop `timestamp` and `metadata`,
    #	and add `author`, `description`, `misc`, `uuid`, `vcs_identifier`
    #	* Adjust required and nullable fields
    #	* Split `Firmware.metadata_item/2` into `fetch_metadata_item/2` and
    #	`get_metadata_item/{2,3}` mirroring `Map.{fetch,get}` semantics
    #	* Update templates and views to respect changes

--- lib/nerves_hub_web/views/firmware_view.ex
+++ lib/nerves_hub_web/views/firmware_view.ex
@@ -1,13 +1,11 @@
 defmodule NervesHubWeb.FirmwareView do
   use NervesHubWeb, :view
 
-  def format_timestamp(timestamp) do
-    date = timestamp |> Timex.format!("{ISOdate}")
-    time = timestamp |> Timex.format!("{h24}:{m}")
-    "#{date} #{time}"
+  def format_signed(%{signed: signed}, _tenant) when signed == false do
+    "No"
   end
 
-  def format_key(%{tenant_key_id: tenant_key_id}, tenant) do
+  def format_signed(%{tenant_key_id: tenant_key_id}, tenant) do
     key = Enum.find(tenant.tenant_keys, &(&1.id == tenant_key_id))
     "#{key.name}"
   end
