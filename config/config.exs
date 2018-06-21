d503dbbfb8e71ee148f724f5c6401f59650b509c
Steven Fuchs <stwf@mac.com>
2018-06-21 17:47:55 -0400

    Optionally disable signups

--- config/config.exs
+++ config/config.exs
@@ -25,6 +25,8 @@ config :logger, :console,
 
 config :nerves_hub, NervesHub.Mailer, adapter: Swoosh.Adapters.Local
 
+config :nerves_hub, NervesHubWeb.AccountController, allow_signups: true
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
--- lib/nerves_hub_web/views/session_view.ex
+++ lib/nerves_hub_web/views/session_view.ex
@@ -1,3 +1,7 @@
 defmodule NervesHubWeb.SessionView do
   use NervesHubWeb, :view
+
+  def permit_uninvited_signups do
+    Application.get_env(:nerves_hub, NervesHubWeb.AccountController)[:allow_signups]
+  end
 end
