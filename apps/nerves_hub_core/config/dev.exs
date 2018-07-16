545e2b9ef5e1055d61999afc7790cbfc69f45cd1
Daniel Spofford <dannyspofford@gmail.com>
2018-07-16 13:13:47 -0500

    Configure distinct db in test
    Why
    ---
    
    * Previously dev and test were sharing the `db` database causing tests
    to fail surprisingly
    
    How
    ---
    
    * Set test-specific `DATABASE_URL` env var in Makefile when running
    tests
    * Remove vestigial (even prior to this commit) `url: ...` and
    `database: ...` mix repo configurations

--- apps/nerves_hub_core/config/dev.exs
+++ apps/nerves_hub_core/config/dev.exs
@@ -2,5 +2,4 @@ use Mix.Config
 
 config :nerves_hub_core, NervesHubCore.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
   ssl: false
--- apps/nerves_hub_core/config/test.exs
+++ apps/nerves_hub_core/config/test.exs
@@ -2,7 +2,5 @@ use Mix.Config
 
 config :nerves_hub_core, NervesHubCore.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
   ssl: false,
-  database: "nerves_hub_test",
   pool: Ecto.Adapters.SQL.Sandbox
