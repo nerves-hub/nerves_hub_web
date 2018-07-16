a9edb2b6ef46e0b8a14d77ad2534145c4f7de476
Jeff McGehee <jlmcgehee21@gmail.com>
2018-07-16 12:56:05 -0400

    Fix paths to seeds.exs
    Why:
    
    * It got overlooked in the switch to the umbrella.
    * `make reset-db` was broken for me, so I figured it was a matter of
    time for others.
    
    This change addresses the need by:
    
    * fixing the path of `seeds.ex`

--- mix.exs
+++ mix.exs
@@ -31,7 +31,11 @@ defmodule NervesHubUmbrella.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": [
+        "ecto.create",
+        "ecto.migrate",
+        "run apps/nerves_hub_core/priv/repo/seeds.exs"
+      ],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
