c38ebe3595a5bc2ea4373043d52f04dde708d31a
Justin Schneck <jschneck@mac.com>
2018-07-17 16:59:42 -0400

    Prevent multiple deployments from executing at the same time.

--- rel/scripts/ecs-deploy.sh
+++ rel/scripts/ecs-deploy.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
 CLUSTER=$1
-SERVICE=$2
-TASK=$3
+SERVICE_NAME=$2
+IMAGE=$3
+MIN_PERCENTAGE=${4:-100}
+MAX_PERCENTAGE=${5:-200}
+REGION="us-east-1"
 
-aws ecs update-service \
-  --service $SERVICE \
-  --cluster $CLUSTER \
-  --task-definition $TASK
+
+if [ -z "$IMAGE" ]; then
+  echo "Please include a image to deploy."
+  exit 1
+fi
+
+echo "Deploying $SERVICE_NAME to $CLUSTER..."
+echo "================="
+ecs-deploy --region $REGION \
+           --cluster $CLUSTER \
+           --service-name $SERVICE_NAME \
+           --image $IMAGE \
+           --timeout 720 \
+           --min $MIN_PERCENTAGE \
+           --max $MAX_PERCENTAGE \
+           --enable-rollback \
+           --max-definitions 50
+echo "================="
