a95f6094b6a381bcfc38084177c25c95649f6beb
Justin Schneck <jschneck@mac.com>
2018-07-25 18:38:04 -0400

    Update prod config for connecting to the ca

--- rel/scripts/s3-sync.sh
+++ rel/scripts/s3-sync.sh
@@ -6,4 +6,4 @@ WORKING_DIR=${WORKING_DIR:-/etc/ssl}
 S3_BUCKET=${S3_BUCKET:-'nerves-hub-ca'}
 
 mkdir -p $WORKING_DIR
-aws s3 sync s3://$S3_BUCKET $WORKING_DIR
+aws s3 sync s3://$S3_BUCKET/ssl $WORKING_DIR
