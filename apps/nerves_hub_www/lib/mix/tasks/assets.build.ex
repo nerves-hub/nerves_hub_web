b519decb997f7460e9f6d3708789aeca0881590f
Justin Schneck <jschneck@mac.com>
2018-08-01 22:22:10 -0400

    Build webpack assets for prod

---
+++ apps/nerves_hub_www/lib/mix/tasks/assets.build.ex
@@ -0,0 +1,14 @@
+defmodule Mix.Tasks.Assets.Build do
+  use Mix.Task
+
+  @shortdoc "Build web assets"
+  @assets Path.expand("../../../assets", __DIR__)
+
+  def run(_) do    
+    System.cmd("yarn", ["build"], [
+      cd: @assets,
+      stderr_to_stdout: true,
+      into: IO.stream(:stdio, :line)
+    ])
+  end
+end
