28409365d00679162fdfe513dbf8b19953993f05
Jeff McGehee <jlmcgehee21@gmail.com>
2018-08-06 14:06:29 -0400

    Allow user cert deletion
    Why:
    
    * We want to delete certs that are bad/stale/etc.
    
    This change addresses the need by:
    
    * adding a delete route and controller.
    * cleaning up the template.
    * minor refactors to controller/router for testing of other controller
    paths.
    
    Other changes:
    * Added a `.tool-versions` file for asdf users (like me :D)

--- apps/nerves_hub_device/config/config.exs
+++ apps/nerves_hub_device/config/config.exs
@@ -10,11 +10,11 @@ config :nerves_hub_device,
   ecto_repos: [NervesHubCore.Repo],
   namespace: NervesHubDevice,
   websocket_auth_methods: [:ssl]
-  
+
 # Configures the endpoint
 config :nerves_hub_device, NervesHubDeviceWeb.Endpoint,
   render_errors: [view: NervesHubWWWWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: NervesHubWeb.PubSub, adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: NervesHubDeviceWeb.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -23,4 +23,4 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
--- apps/nerves_hub_device/lib/nerves_hub_device_web/channels/device_channel.ex
+++ apps/nerves_hub_device/lib/nerves_hub_device_web/channels/device_channel.ex
@@ -2,6 +2,7 @@ defmodule NervesHubDeviceWeb.DeviceChannel do
   use NervesHubDeviceWeb, :channel
 
   alias NervesHubCore.{Devices, Firmwares, Accounts, Deployments}
+  alias NervesHubDeviceWeb.Presence
 
   @uploader Application.get_env(:nerves_hub_www, :firmware_upload)
 
@@ -63,17 +64,10 @@ defmodule NervesHubDeviceWeb.DeviceChannel do
 
   defp do_update_message(_, _), do: {:error, :unknown_error}
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (device:lobby).
-  def handle_in("shout", payload, socket) do
-    broadcast(socket, "shout", payload)
-    {:noreply, socket}
+  def online?(%Devices.Device{} = device) do
+    device
+    |> (fn d -> "devices#{device.identifier}" end).()
+    |> Presence.list()
   end
 
   # Add authorization logic here as required.
