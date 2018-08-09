b0c58d17e5c4314d783f6f330cd384f39594d60b
Justin Schneck <jschneck@mac.com>
2018-08-08 22:08:56 -0400

    Increase assert receive timeout in device tests

--- apps/nerves_hub_device/test/test_helper.exs
+++ apps/nerves_hub_device/test/test_helper.exs
@@ -1,5 +1,9 @@
 Logger.remove_backend(:console)
 
-ExUnit.start()
+assert_timeout = String.to_integer(
+  System.get_env("ELIXIR_ASSERT_TIMEOUT") || "200"
+)
+
+ExUnit.start(assert_receive_timeout: assert_timeout)
 
 Ecto.Adapters.SQL.Sandbox.mode(NervesHubCore.Repo, :manual)
