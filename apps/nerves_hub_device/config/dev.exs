6772db71c6702bc10f36016df693cef169fbf6d8
Justin Schneck <jschneck@mac.com>
2018-08-08 21:35:43 -0400

    Start he pubsub adapter in NervesHubCore

--- apps/nerves_hub_device/config/dev.exs
+++ apps/nerves_hub_device/config/dev.exs
@@ -11,7 +11,6 @@ config :nerves_hub_device, NervesHubDeviceWeb.Endpoint,
   code_reloader: false,
   check_origin: false,
   watchers: [],
-  pubsub: [name: NervesHubWeb.PubSub, adapter: Phoenix.PubSub.PG2],
   https: [
     port: 4001,
     otp_app: :nerves_hub_device,
