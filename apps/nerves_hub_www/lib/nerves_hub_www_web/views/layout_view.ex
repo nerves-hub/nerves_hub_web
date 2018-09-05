57026bcbc3fda6535ea9ca79ad06ad94f881b51b
Jeff McGehee <jlmcgehee21@gmail.com>
2018-09-04 19:28:01 -0700

    Fix logo href
    Why:
    
    * It was sending you to the `home_path` when it shouldn't
    
    This change addresses the need by:
    
    * Adding a `logo_href` function in the layout view

--- apps/nerves_hub_www/lib/nerves_hub_www_web/views/layout_view.ex
+++ apps/nerves_hub_www/lib/nerves_hub_www_web/views/layout_view.ex
@@ -13,6 +13,14 @@ defmodule NervesHubWWWWeb.LayoutView do
   def logged_in?(%{assigns: %{user: %User{}}}), do: true
   def logged_in?(_), do: false
 
+  def logo_href(conn) do
+    if logged_in?(conn) do
+      dashboard_path(conn, :index)
+    else
+      home_path(conn, :index)
+    end
+  end
+
   def permit_uninvited_signups do
     Application.get_env(:nerves_hub_www, NervesHubWWWWeb.AccountController)[:allow_signups]
   end
