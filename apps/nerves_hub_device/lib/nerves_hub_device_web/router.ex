d3dd153d05dc25ec9b67d82fc7eb79ce8da1e227
Connor Rigby <konnorrigby@gmail.com>
2018-10-02 12:00:53 -0700

    Add endpoints for a device to consume.

--- apps/nerves_hub_device/lib/nerves_hub_device_web/router.ex
+++ apps/nerves_hub_device/lib/nerves_hub_device_web/router.ex
@@ -1,6 +1,10 @@
 defmodule NervesHubDeviceWeb.Router do
   use NervesHubDeviceWeb, :router
 
+  pipeline :device do
+    plug(NervesHubDeviceWeb.Plugs.Device)
+  end
+
   pipeline :api do
     plug(:accepts, ["json"])
   end
@@ -8,5 +12,11 @@ defmodule NervesHubDeviceWeb.Router do
   # Other scopes may use custom stacks.
   scope "/", NervesHubDeviceWeb do
     pipe_through(:api)
+    pipe_through(:device)
+
+    scope "/device" do
+      get("/me", DeviceController, :me)
+      get("/update", DeviceController, :update)
+    end
   end
 end
---
+++ apps/nerves_hub_device/test/nerves_hub_device_web/controllers/device_controller_test.exs
@@ -0,0 +1,31 @@
+defmodule NervesHubDeviceWeb.DeviceControllerTest do
+  use NervesHubDeviceWeb.ConnCase, async: true
+  alias NervesHubCore.{Deployments, Fixtures}
+
+  describe "device" do
+    test "identify a device", %{conn: conn, device: device} do
+      conn = get(conn, device_path(conn, :me))
+      assert json_response(conn, 200)["data"]["identifier"] == device.identifier
+    end
+
+    test "gets available update", %{
+      conn: conn,
+      org_key: org_key,
+      deployment: deployment,
+      product: product
+    } do
+      # Make sure there is no update available
+      conn = get(conn, device_path(conn, :update))
+      assert json_response(conn, 200)["data"]["update_available"] == false
+
+      # Create a new firmware and active deployment for the device
+      new_firmware = Fixtures.firmware_fixture(org_key, product)
+      params = %{firmware_id: new_firmware.id, is_active: true}
+      {:ok, _} = Deployments.update_deployment(deployment, params)
+
+      # Should be an update available now.
+      conn = get(conn, device_path(conn, :update))
+      assert json_response(conn, 200)["data"]["update_available"]
+    end
+  end
+end
