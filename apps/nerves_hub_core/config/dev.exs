440776af425289df362f5842290d7a94028bb048
Connor Rigby <konnorrigby@gmail.com>
2018-10-12 11:55:01 -0700

    Fix wrong path for local firmware downloads
    This was a typo from a [previous
    commit](https://github.com/nerves-hub/nerves_hub_web/commit/d92b0babc7dbc5d48d988ff42cacedbc8f318fe1#diff-e9891a4e21a8d1476ca0381fe60a454fL63)

--- apps/nerves_hub_core/config/dev.exs
+++ apps/nerves_hub_core/config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :nerves_hub_core, firmware_upload: NervesHubCore.Firmwares.Upload.File
 
 config :nerves_hub_core, NervesHubCore.Firmwares.Upload.File,
-  local_path: System.tmp_dir(),
+  local_path: Path.join(System.tmp_dir(), "firmware"),
   public_path: "/firmware"
 
 # config :nerves_hub_core, NervesHubCore.Firmwares.Upload.S3, bucket: System.get_env("S3_BUCKET_NAME")
