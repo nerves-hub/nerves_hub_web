8c78932b4bae3cc4b38ed409ecc3a37149f45cbb
Justin Schneck <jschneck@mac.com>
2018-11-01 15:40:03 -0400

    Get environment variables from aws parameter store

--- rel/scripts/docker-entrypoint.sh
+++ rel/scripts/docker-entrypoint.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+export AWS_ENV_PATH="/$APP_NAME/$ENVIRONMENT/"
+
+# Set env vars from AWS SSM
+# This uses get-parameters-from-path and automatically sets any that match the prefix above (AWS_ENV_PATH)
+# http://docs.aws.amazon.com/cli/latest/reference/ssm/get-parameters-by-path.html
+eval $(aws-env)
+
 SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 $SCRIPT_PATH/s3-sync.sh
