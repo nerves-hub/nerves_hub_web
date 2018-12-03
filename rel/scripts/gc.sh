3cbb1913d706a459d9f2e4e8b967b54ada97f840
Justin Schneck <jschneck@mac.com>
2018-12-03 15:18:18 -0500

    Ensure repositories and apps are started before trying to gc firmware

--- rel/scripts/gc.sh
+++ rel/scripts/gc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "Starting GC Firmware"
-$RELEASE_ROOT_DIR/bin/nerves_hub_www command Elixir.NervesHubWebCore.Firmwares.GC run
-echo "Finished GC Firmware"
+echo "Starting GC"
+$RELEASE_ROOT_DIR/bin/nerves_hub_www command Elixir.NervesHubWebCore.Release.Tasks gc
+echo "Finished GC"
