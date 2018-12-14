5f430be4c371a95695f7595df3aa7eeabdc4c6c0
Justin Schneck <jschneck@mac.com>
2018-12-14 10:27:44 -0500

    Enable fail if no peer cert for device endpoints

--- apps/nerves_hub_device/config/test.exs
+++ apps/nerves_hub_device/config/test.exs
@@ -14,6 +14,7 @@ config :nerves_hub_device, NervesHubDeviceWeb.Endpoint,
     # Enable client SSL
     verify: :verify_peer,
     verify_fun: {&NervesHubDevice.SSL.verify_fun/3, nil},
+    fail_if_no_peer_cert: true,
     keyfile: Path.join([__DIR__, "../../../test/fixtures/ssl/device.nerves-hub.org-key.pem"]),
     certfile: Path.join([__DIR__, "../../../test/fixtures/ssl/device.nerves-hub.org.pem"]),
     cacertfile: Path.join([__DIR__, "../../../test/fixtures/ssl/ca.pem"])
