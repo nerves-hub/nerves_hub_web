a997de060a65450cdae655a9fe17b0d37c93ffb0
Justin Schneck <jschneck@mac.com>
2018-12-14 10:31:21 -0500

    Fix test port numbers
    [skip ci]

--- apps/nerves_hub_www/config/test.exs
+++ apps/nerves_hub_www/config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :nerves_hub_www, NervesHubWWWWeb.Endpoint,
-  http: [port: 4001],
+  http: [port: 4000],
   server: false
 
 # Print only warnings and errors during test
