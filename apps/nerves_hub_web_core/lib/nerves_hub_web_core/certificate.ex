5a810bd3cc4e6a3376039343e77ed7960cfdf2d8
Justin Schneck <jschneck@mac.com>
2019-01-04 11:20:23 -0500

    Add support for generalized time validity timestamps

--- apps/nerves_hub_web_core/lib/nerves_hub_web_core/certificate.ex
+++ apps/nerves_hub_web_core/lib/nerves_hub_web_core/certificate.ex
@@ -65,21 +65,44 @@ defmodule NervesHubWebCore.Certificate do
       X509.Certificate.validity(otp_certificate)
       |> validity()
 
-    {:utcTime, not_before} = Keyword.get(validity, :notBefore)
-    {:utcTime, not_after} = Keyword.get(validity, :notAfter)
+    {type, not_before} = Keyword.get(validity, :notBefore)
+    not_before = convert_timestamp({type, to_string(not_before)})
 
-    not_before = to_string(not_before) |> convert_generalized_time()
-    not_after = to_string(not_after) |> convert_generalized_time()
+    {type, not_after} = Keyword.get(validity, :notAfter)
+    not_after = convert_timestamp({type, to_string(not_after)})
+
     {not_before, not_after}
   end
 
-  defp convert_generalized_time(timestamp) do
+  defp convert_timestamp({:utcTime, timestamp}) do
     <<year::binary-unit(8)-size(2), month::binary-unit(8)-size(2), day::binary-unit(8)-size(2),
       hour::binary-unit(8)-size(2), minute::binary-unit(8)-size(2),
       second::binary-unit(8)-size(2), "Z">> = timestamp
 
     NaiveDateTime.new(
       String.to_integer(year) + @era,
+      String.to_integer(month),
+      String.to_integer(day),
+      String.to_integer(hour),
+      String.to_integer(minute),
+      String.to_integer(second)
+    )
+    |> case do
+      {:ok, naive_date_time} ->
+        DateTime.from_naive!(naive_date_time, "Etc/UTC")
+
+      error ->
+        error
+    end
+  end
+
+  defp convert_timestamp({:generalTime, timestamp}) do
+    <<year::binary-unit(8)-size(4), month::binary-unit(8)-size(2), day::binary-unit(8)-size(2),
+      hour::binary-unit(8)-size(2), minute::binary-unit(8)-size(2),
+      second::binary-unit(8)-size(2), "Z">> = timestamp
+
+    NaiveDateTime.new(
+      String.to_integer(year),
       String.to_integer(month),
       String.to_integer(day),
       String.to_integer(hour),
