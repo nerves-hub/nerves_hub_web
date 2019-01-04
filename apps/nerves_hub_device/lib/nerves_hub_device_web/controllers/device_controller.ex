3a49ab82149f234d7e9256d9235c84c3a79bf4d1
connor rigby <konnorrigby@gmail.com>
2019-01-04 10:58:57 -0800

    Update devices when tags are updated.
    Fixes #309

--- apps/nerves_hub_device/lib/nerves_hub_device_web/controllers/device_controller.ex
+++ apps/nerves_hub_device/lib/nerves_hub_device_web/controllers/device_controller.ex
@@ -1,8 +1,6 @@
 defmodule NervesHubDeviceWeb.DeviceController do
   use NervesHubDeviceWeb, :controller
-  alias NervesHubWebCore.{Devices, Firmwares, Deployments}
-
-  @uploader Application.get_env(:nerves_hub_web_core, :firmware_upload)
+  alias NervesHubWebCore.Devices
 
   def me(%{assigns: %{device: device}} = conn, _params) do
     render(conn, "show.json", device: device)
@@ -10,18 +8,7 @@ defmodule NervesHubDeviceWeb.DeviceController do
 
   def update(%{assigns: %{device: device}} = conn, _params) do
     deployments = Devices.get_eligible_deployments(device)
-    join_reply = resolve_update(device.org, deployments)
+    join_reply = Devices.resolve_update(device.org, deployments)
     render(conn, "update.json", reply: join_reply)
-  end
-
-  defp resolve_update(_org, _deployments = []), do: %{update_available: false}
-
-  defp resolve_update(org, [%Deployments.Deployment{} = deployment | _]) do
-    with {:ok, firmware} <- Firmwares.get_firmware(org, deployment.firmware_id),
-         {:ok, url} <- @uploader.download_file(firmware) do
-      %{update_available: true, firmware_url: url}
-    else
-      _ -> %{update_available: false}
-    end
   end
 end
