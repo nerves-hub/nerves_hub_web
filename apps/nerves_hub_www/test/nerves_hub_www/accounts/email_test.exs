3b75a386f37f729c20421a2756c08aced35fb1dd
Jeff McGehee <jlmcgehee21@gmail.com>
2019-01-18 16:22:21 -0500

    Basic email rendering tests
    Why:
    
    * We had none.
    * https://github.com/nerves-hub/nerves_hub_web/issues/333

---
+++ apps/nerves_hub_www/test/nerves_hub_www/accounts/email_test.exs
@@ -0,0 +1,34 @@
+defmodule NervesHubWWW.Accounts.EmailTest do
+  use ExUnit.Case, async: true
+
+  alias NervesHubWebCore.Accounts.{Invite, Org, User}
+  alias NervesHubWWW.Accounts.Email
+  alias NervesHubWWWWeb.EmailView
+
+  test "invite email" do
+    invite = %Invite{email: "foo@bar.com"}
+    org = %Org{name: "My Org Name"}
+
+    email = Email.invite(invite, org)
+
+    assert email.to == invite.email
+    assert email.html_body =~ org.name
+    assert email.html_body =~ "#{EmailView.base_url()}/invite/#{invite.token}"
+  end
+
+  test "forgot_password email" do
+    user = %User{
+      username: "sad_guy",
+      email: "sad_guy@forgot_password.com",
+      password_reset_token: "ultrarandomresettoken"
+    }
+
+    email = Email.forgot_password(user)
+
+    assert email.to == user.email
+    assert email.html_body =~ user.username
+
+    assert email.html_body =~
+             "#{EmailView.base_url()}/password-reset/#{user.password_reset_token}"
+  end
+end
