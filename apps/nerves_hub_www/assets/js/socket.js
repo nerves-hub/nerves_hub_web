cf90e9163eda66bd934b61588ea5fdf162d757fb
Daniel Spofford <dannyspofford@gmail.com>
2019-01-27 18:03:02 -0600

    Fix last_communication bug and test js in CI
    Why
    ---
    
    * Fixes a bug where if a device has no `last_communication` it was
      errantly parsed into `Invalid Date`.
    
    How
    ---
    
    * Update date formatting logic to account for `never` value.
    * Test date formatting logic.
    * Add an eslint config.
    * Add a prettier config.
    * Test JS, verify formatting, and lint JS in circleci.
    * Remove comments from and format socket js file. Also remove
      unreferenced `resp` variables.

--- apps/nerves_hub_www/assets/js/socket.js
+++ apps/nerves_hub_www/assets/js/socket.js
@@ -1,83 +1,34 @@
-// NOTE: The contents of this file will only be executed if
-// you uncomment its entry in "assets/js/app.js".
+import { Presence, Socket } from 'phoenix'
 
-// To use Phoenix channels, the first step is to import Socket,
-// and connect at the socket path in "lib/web/endpoint.ex".
-//
-// Pass the token on params as below. Or remove it
-// from the params if you are not using authentication.
-import {Presence, Socket} from "phoenix"
+let socket = new Socket('/socket', { params: { token: window.userToken } })
 
-let socket = new Socket("/socket", {params: {token: window.userToken}})
-
-// When you connect, you'll often need to authenticate the client.
-// For example, imagine you have an authentication plug, `MyAuth`,
-// which authenticates the session and assigns a `:current_user`.
-// If the current user exists you can assign the user's token in
-// the connection for use in the layout.
-//
-// In your "lib/web/router.ex":
-//
-//     pipeline :browser do
-//       ...
-//       plug MyAuth
-//       plug :put_user_token
-//     end
-//
-//     defp put_user_token(conn, _) do
-//       if current_user = conn.assigns[:current_user] do
-//         token = Phoenix.Token.sign(conn, "user socket", current_user.id)
-//         assign(conn, :user_token, token)
-//       else
-//         conn
-//       end
-//     end
-//
-// Now you need to pass this token to JavaScript. You can do so
-// inside a script tag in "lib/web/templates/layout/app.html.eex":
-//
-//     <script>window.userToken = "<%= assigns[:user_token] %>";</script>
-//
-// You will need to verify the user token in the "connect/2" function
-// in "lib/web/channels/user_socket.ex":
-//
-//     def connect(%{"token" => token}, socket) do
-//       # max_age: 1209600 is equivalent to two weeks in seconds
-//       case Phoenix.Token.verify(socket, "user socket", token, max_age: 1209600) do
-//         {:ok, user_id} ->
-//           {:ok, assign(socket, :user, user_id)}
-//         {:error, reason} ->
-//           :error
-//       end
-//     end
-//
-// Finally, connect to the socket:
 socket.connect()
 
-// Now that you are connected, you can join channels with a topic:
 let channel = socket.channel(`devices:${window.orgId}`, {})
-
 let presences = {}
 
-let updateStatuses = (presences) => {
-  document.querySelectorAll(".device").forEach((d) => {
-    const {[d.dataset.deviceId]: {status} = {status: 'offline'}} = presences
+let updateStatuses = presences => {
+  document.querySelectorAll('.device').forEach(d => {
+    const {
+      [d.dataset.deviceId]: { status } = { status: 'offline' }
+    } = presences
     d.innerHTML = status
   })
 }
 
-channel.on("presence_state", state => {
+channel.on('presence_state', state => {
   presences = Presence.syncState(presences, state)
   updateStatuses(presences)
 })
 
-channel.on("presence_diff", diff => {
+channel.on('presence_diff', diff => {
   presences = Presence.syncDiff(presences, diff)
   updateStatuses(presences)
 })
 
-channel.join()
-  .receive("ok", resp => {})
-  .receive("error", resp => {})
+channel
+  .join()
+  .receive('ok', () => {})
+  .receive('error', () => {})
 
 export default socket
