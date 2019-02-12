6cc8c51003a863985d1463bb8f3b27097eab8446
Justin Schneck <jschneck@mac.com>
2019-02-12 15:41:20 -0500

    Add quantum and schedule firmware job

--- apps/nerves_hub_web_core/config/prod.exs
+++ apps/nerves_hub_web_core/config/prod.exs
@@ -3,6 +3,15 @@ use Mix.Config
 config :ex_aws,
   region: "${AWS_REGION}"
 
+config :nerves_hub_web_core, NervesHubWebCore.Scheduler,
+  global: true,
+  jobs: [
+    garbage_collect_firmware: [
+      schedule: "*/15 * * * *",
+      task: {NervesHubWebCore.Firmwares.GC, :run, []}
+    ]
+  ]
+
 config :nerves_hub_web_core, firmware_upload: NervesHubWebCore.Firmwares.Upload.S3
 config :nerves_hub_web_core, NervesHubWebCore.Firmwares.Upload.S3, bucket: "${S3_BUCKET_NAME}"
 
