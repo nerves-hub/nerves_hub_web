39364883d7de9169ed976443910458f1031ac89e
Justin Schneck <jschneck@mac.com>
2019-02-12 16:25:14 -0500

    Do not remove last_known_firmware_id until data has been migrated.

--- apps/nerves_hub_web_core/priv/repo/migrations/20190209173245_add_device_firmware_metadata_remove_last_link.exs
+++ apps/nerves_hub_web_core/priv/repo/migrations/20190209173245_add_device_firmware_metadata_remove_last_link.exs
@@ -4,7 +4,6 @@ defmodule NervesHubWebCore.Repo.Migrations.AddDeviceFirmwareMetadataRemoveLastLi
   def change do
     alter table(:devices) do
       add :firmware_metadata, :map
-      remove :last_known_firmware_id
     end
   end
 end
