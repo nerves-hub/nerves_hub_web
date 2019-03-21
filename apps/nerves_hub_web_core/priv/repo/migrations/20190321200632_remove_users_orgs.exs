7247253310e2a3a39eb1c64db334c93fc75876b4
Justin Schneck <jschneck@mac.com>
2019-03-21 16:11:37 -0400

    Remove old users_orgs join table

---
+++ apps/nerves_hub_web_core/priv/repo/migrations/20190321200632_remove_users_orgs.exs
@@ -0,0 +1,8 @@
+defmodule NervesHubWebCore.Repo.Migrations.RemoveUsersOrgs do
+  use Ecto.Migration
+
+  def change do
+    drop(index(:users_orgs, [:user_id, :org_id]))
+    drop(table(:users_orgs))
+  end
+end
