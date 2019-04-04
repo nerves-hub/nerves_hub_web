42ba06f46645a92e7ad21bac9836a716af180097
Jon Carstens <jjcarstens@me.com>
2019-04-04 10:48:47 -0600

    Display more info for Account Certificates * Adjusts `account_certificates#index` html to display more info about certs * adjusts date.js to fix displaying dates in Safari
    Fixes https://github.com/nerves-hub/nerves_hub_web/issues/389
    Fixes https://github.com/nerves-hub/nerves_hub_web/issues/347

--- apps/nerves_hub_www/assets/js/dates.test.js
+++ apps/nerves_hub_www/assets/js/dates.test.js
@@ -1,20 +1,20 @@
 const dates = require('./dates')
 
-describe('formatLastCommunication', () => {
+describe('formatDateTime', () => {
   test('formats ISO 8601 dates', () => {
     let date = new Date()
 
     date.setMilliseconds(0)
-    expect(
-      new Date(dates.formatLastCommunication(date.toISOString())).getTime()
-    ).toBe(date.getTime())
+    expect(new Date(dates.formatDateTime(date.toISOString())).getTime()).toBe(
+      date.getTime()
+    )
   })
 
   test('preserves "never" value', () => {
-    expect(dates.formatLastCommunication('never')).toBe('never')
+    expect(dates.formatDateTime('never')).toBe('never')
   })
 
   test('handles white space around "never" value', () => {
-    expect(dates.formatLastCommunication(' never  ')).toBe('never')
+    expect(dates.formatDateTime(' never  ')).toBe('never')
   })
 })
