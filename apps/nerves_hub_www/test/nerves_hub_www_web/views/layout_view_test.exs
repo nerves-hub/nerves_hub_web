6579e5ac180cc896e14b97902846e4d8a0440192
Peter C. Marks <peter.c.marks@gmail.com>
2019-04-10 09:23:00 -0400

    Humanize display of organization info
    Fixes #415
    Show Firmware size in MiB, GiB
    Show TTL in days, etc. depending on value.
    Placed in LayoutView.
    Added unit tests.

---
+++ apps/nerves_hub_www/test/nerves_hub_www_web/views/layout_view_test.exs
@@ -0,0 +1,23 @@
+defmodule NervesHubWWWWeb.LayoutViewTest do
+  use ExUnit.Case
+
+  import NervesHubWWWWeb.LayoutView, only: [humanize_size: 1, humanize_seconds: 1]
+
+  describe "Test humanize functions" do
+    test "humanize size" do
+      result = humanize_size(1_167_772_160)
+      assert result == "1.088 GiB"
+      result = humanize_size(167_772_160)
+      assert result == "160.0 MiB"
+      result = humanize_size(772_160)
+      assert result == "754.063 KiB"
+    end
+
+    test "humanize seconds" do
+      result = humanize_seconds(3456)
+      assert result == "57 minutes, 36 seconds"
+      result = humanize_seconds(60 * 60 * 24 * 7)
+      assert result == "1 week"
+    end
+  end
+end
