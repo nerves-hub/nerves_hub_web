503fbd38b672d0112d9528451e43b80699e2899f
Peter C. Marks <peter.c.marks@gmail.com>
2019-04-19 10:33:59 -0400

    Add unit test for CA certs display

---
+++ apps/nerves_hub_www/test/nerves_hub_www_web/controllers/org_certificate_controller_test.exs
@@ -0,0 +1,20 @@
+defmodule NervesHubWWWWeb.OrgCertificateControllerTest do
+  use NervesHubWWWWeb.ConnCase.Browser, async: true
+
+  alias NervesHubWebCore.Fixtures
+
+  describe "index" do
+    test "lists all appropriate device(ca) certificates", %{
+      conn: conn,
+      current_org: org
+    } do
+      %{db_cert: db1_cert} = Fixtures.ca_certificate_fixture(org)
+      %{db_cert: db2_cert} = Fixtures.ca_certificate_fixture(org)
+
+      conn = get(conn, org_certificate_path(conn, :index))
+      assert html_response(conn, 200) =~ "#{org.name} Device (CA) Certificates"
+      assert html_response(conn, 200) =~ db1_cert.serial
+      assert html_response(conn, 200) =~ db2_cert.serial
+    end
+  end
+end
