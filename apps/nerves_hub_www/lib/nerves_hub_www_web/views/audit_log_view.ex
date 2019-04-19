797a36c4abd508afbcf0234dc0776e0aae34e739
Justin Schneck <jschneck@mac.com>
2019-04-19 18:11:10 -0400

    Fix comiler warnings

--- apps/nerves_hub_www/lib/nerves_hub_www_web/views/audit_log_view.ex
+++ apps/nerves_hub_www/lib/nerves_hub_www_web/views/audit_log_view.ex
@@ -1,7 +1,7 @@
 defmodule NervesHubWWWWeb.AuditLogView do
   use NervesHubWWWWeb, :view
 
-  import NervesHubWWWWeb.LayoutView, only: [pagination_links: 1, pagination_links: 2]
+  import NervesHubWWWWeb.LayoutView, only: [pagination_links: 2]
 
   def actor_link(%{actor_id: id, actor_type: type}, current_id) do
     link_to_resource(type, id, current_id, "audit-log-actor")
@@ -36,7 +36,7 @@ defmodule NervesHubWWWWeb.AuditLogView do
     |> Enum.at(-1)
   end
 
-  defp link_to_resource(type, id, current_id, class \\ "") do
+  defp link_to_resource(type, id, current_id, class) do
     simple_type = feed_type(type)
 
     if id == current_id do
@@ -52,12 +52,12 @@ defmodule NervesHubWWWWeb.AuditLogView do
     "/products"
   end
 
-  def path_for("firmware", id) do
+  def path_for("firmware", _id) do
     # TODO: handle product id lookup
     "/products"
   end
 
-  def path_for("user", id) do
+  def path_for("user", _id) do
     # TODO: Add route for user#show instead of settings
     ""
   end
