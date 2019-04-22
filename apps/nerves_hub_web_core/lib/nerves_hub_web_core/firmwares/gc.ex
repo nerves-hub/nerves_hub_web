8cfecc744c01004c15ebec98ee621faa5a70ba93
Justin Schneck <jschneck@mac.com>
2019-04-22 14:33:18 -0400

    Use Firmwares.delete_firmware/1 when garbage collecting

--- apps/nerves_hub_web_core/lib/nerves_hub_web_core/firmwares/gc.ex
+++ apps/nerves_hub_web_core/lib/nerves_hub_web_core/firmwares/gc.ex
@@ -1,14 +1,13 @@
 defmodule NervesHubWebCore.Firmwares.GC do
   require Logger
 
-  alias NervesHubWebCore.Repo
   alias NervesHubWebCore.Firmwares
 
   def run() do
     Firmwares.get_firmware_by_expired_ttl()
     |> Enum.each(fn firmware ->
-      case Repo.delete(firmware) do
-        {:ok, _firmware} ->
+      case Firmwares.delete_firmware(firmware) do
+        :ok ->
           Logger.debug("Garbage collected firmware #{firmware.uuid}")
 
         {:error, reason} ->
