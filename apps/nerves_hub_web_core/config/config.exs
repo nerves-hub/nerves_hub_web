b5a3dae3e07ed240224473180ca76f5517c9b74c
Jon Carstens <jjcarstens@me.com>
2019-06-03 13:28:41 -0600

    Set ex_aws json_codec to use Jason

--- apps/nerves_hub_web_core/config/config.exs
+++ apps/nerves_hub_web_core/config/config.exs
@@ -12,7 +12,10 @@ config :nerves_hub_web_core, NervesHubWeb.PubSub,
 
 config :ex_aws,
   access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
+  json_codec: Jason,
   secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
   region: System.get_env("AWS_REGION")
+
+config :ex_aws_s3, json_codec: Jason
 
 import_config "#{Mix.env()}.exs"
