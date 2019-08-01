9d726f22c945c0f41d1cd76442a24645b4fe6797
Justin Schneck <jschneck@mac.com>
2019-08-01 18:10:19 -0400

    do not require rollbar token

--- apps/nerves_hub_www/config/release.exs
+++ apps/nerves_hub_www/config/release.exs
@@ -18,7 +18,11 @@ config :kernel,
   inet_dist_listen_min: 9100,
   inet_dist_listen_max: 9155
 
-config :rollbax, access_token: System.fetch_env!("ROLLBAR_ACCESS_TOKEN")
+if rollbar_access_token = System.get_env("ROLLBAR_ACCESS_TOKEN") do
+  config :rollbax, access_token: rollbar_access_token
+else
+  config :rollbax, enabled: false
+end
 
 config :nerves_hub_web_core, NervesHubWebCore.Firmwares.Upload.S3,
   bucket: System.fetch_env!("S3_BUCKET_NAME")
