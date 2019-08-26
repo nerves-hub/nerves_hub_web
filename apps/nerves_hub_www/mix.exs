4b24f6b609255af08b34920b12c0621e3735ceea
Justin Schneck <jschneck@mac.com>
2019-08-26 11:24:08 -0400

    phoenix_live_view: bump to v0.1.0

--- apps/nerves_hub_www/mix.exs
+++ apps/nerves_hub_www/mix.exs
@@ -56,7 +56,7 @@ defmodule NervesHubWWW.MixProject do
       {:phoenix, "~> 1.4"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
+      {:phoenix_live_view, "~> 0.1"},
       {:plug, "~> 1.7"},
       {:plug_cowboy, "~> 2.0"},
       {:phoenix_ecto, "~> 4.0"},
