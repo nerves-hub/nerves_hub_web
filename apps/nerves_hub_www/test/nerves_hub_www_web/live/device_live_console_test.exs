9a7579ed1abbd251a426853016be7f50757e6489
Jon Carstens <jjcarstens@me.com>
2019-08-26 21:31:46 -0600

    update live view tests for 0.1.0

--- apps/nerves_hub_www/test/nerves_hub_www_web/live/device_live_console_test.exs
+++ apps/nerves_hub_www/test/nerves_hub_www_web/live/device_live_console_test.exs
@@ -4,21 +4,13 @@ defmodule NervesHubWWWWeb.DeviceLiveConsoleTest do
   import Phoenix.ChannelTest
 
   alias NervesHubWWWWeb.Router.Helpers, as: Routes
-  alias NervesHubWWWWeb.{DeviceLive.Console, Endpoint}
+  alias NervesHubWWWWeb.Endpoint
   alias NervesHubDevice.Presence
 
   alias Phoenix.Socket.Broadcast
 
-  setup %{fixture: %{org: org, device: device, product: product}} = context do
+  setup %{fixture: %{device: device, product: product}} = context do
     Endpoint.subscribe("console:#{device.id}")
-
-    # TODO: Use Plug.Conn.get_session/1 when upgraded to Plug >= 1.8
-    session =
-      context.conn.private.plug_session
-      |> Enum.into(%{}, fn {k, v} -> {String.to_atom(k), v} end)
-      |> Map.put(:org_id, org.id)
-      |> Map.put(:product_id, product.id)
-      |> Map.put(:device_id, device.id)
 
     unless context[:skip_presence] do
       Presence.track(self(), "product:#{product.id}:devices", device.id, %{
@@ -26,39 +18,35 @@ defmodule NervesHubWWWWeb.DeviceLiveConsoleTest do
       })
     end
 
-    [session: session]
+    :ok
   end
 
   describe "mount" do
-    test "successful mount init IEx server on device", %{session: session} do
-      {:ok, _view, _html} = mount(Endpoint, Console, session: session)
+    test "successful mount init IEx server on device", %{conn: conn, fixture: fixture} do
+      {:ok, _view, _html} = live(conn, device_path(fixture, :console))
       assert_broadcast("init", %{})
     end
 
     @tag skip_presence: true
-    test "redirects when device not configured for remote IEx", %{
-      fixture: %{org: org, product: product, device: device},
-      session: session
-    } do
-      {:error, %{redirect: somewhere}} = mount(Endpoint, Console, session: session)
+    test "redirects when device not configured for remote IEx", %{conn: conn, fixture: fixture} do
+      path = device_path(fixture, :show)
+      assert {:error, %{redirect: %{to: ^path}}} = live(conn, device_path(fixture, :console))
       refute_broadcast("init", %{})
-      path = Routes.device_path(Endpoint, :show, org.name, product.name, device.identifier)
-      assert somewhere == path
     end
 
-    test "redirects on mount with unrecognized session structure" do
+    test "redirects on mount with unrecognized session structure", %{conn: conn, fixture: fixture} do
       home_path = Routes.home_path(Endpoint, :index)
+      conn = clear_session(conn)
 
-      assert {:error, %{redirect: ^home_path}} =
-               mount(Endpoint, Console, session: "wat?! who der?!")
+      assert {:error, %{redirect: %{to: ^home_path}}} = live(conn, device_path(fixture, :console))
     end
   end
 
   describe "handle_event" do
-    test "iex_submit", %{user: user, session: session} do
-      {:ok, view, _html} = mount(Endpoint, Console, session: session)
+    test "iex_submit", %{conn: conn, fixture: fixture} do
+      {:ok, view, _html} = live(conn, device_path(fixture, :console))
       input = "Howdy"
-      iex_line = "iex(#{user.username})&gt; #{input}"
+      iex_line = "iex(#{fixture.user.username})&gt; #{input}"
 
       assert render_submit(view, :iex_submit, %{iex_input: input}) =~ iex_line
 
@@ -66,16 +54,16 @@ defmodule NervesHubWWWWeb.DeviceLiveConsoleTest do
       assert_broadcast("io_reply", %{data: input, kind: "get_line"})
     end
 
-    test "iex_submit - clear text", %{session: session} do
-      {:ok, view, _html} = mount(Endpoint, Console, session: session)
+    test "iex_submit - clear text", %{conn: conn, fixture: fixture} do
+      {:ok, view, _html} = live(conn, device_path(fixture, :console))
 
       refute render_submit(view, :iex_submit, %{iex_input: "clear"}) =~ "NervesHub IEx Live"
     end
   end
 
   describe "handle_info" do
-    test "put_chars", %{session: session} do
-      {:ok, view, html} = mount(Endpoint, Console, session: session)
+    test "put_chars", %{conn: conn, fixture: fixture} do
+      {:ok, view, html} = live(conn, device_path(fixture, :console))
 
       refute html =~ "Howdy"
 
@@ -85,10 +73,10 @@ defmodule NervesHubWWWWeb.DeviceLiveConsoleTest do
       assert render(view) =~ "Howdy"
     end
 
-    test "put_chars with binary list", %{session: session} do
+    test "put_chars with binary list", %{conn: conn, fixture: fixture} do
       list_line = [72, 111, 119, 100, 121, 32, 'Partner']
 
-      {:ok, view, html} = mount(Endpoint, Console, session: session)
+      {:ok, view, html} = live(conn, device_path(fixture, :console))
 
       refute html =~ "Howdy Partner"
 
@@ -98,19 +86,19 @@ defmodule NervesHubWWWWeb.DeviceLiveConsoleTest do
       assert render(view) =~ "Howdy Partner"
     end
 
-    test "get_line", %{user: user, session: session} do
-      {:ok, view, html} = mount(Endpoint, Console, session: session)
+    test "get_line", %{conn: conn, fixture: fixture} do
+      {:ok, view, html} = live(conn, device_path(fixture, :console))
 
       refute html =~ "Howdy"
 
       msg = %Broadcast{event: "get_line", payload: %{"data" => "iex(1)>"}}
       send(view.pid, msg)
 
-      assert render(view) =~ "iex(#{user.username})&gt;"
+      assert render(view) =~ "iex(#{fixture.user.username})&gt;"
     end
 
-    test "add_line", %{session: session} do
-      {:ok, view, html} = mount(Endpoint, Console, session: session)
+    test "add_line", %{conn: conn, fixture: fixture} do
+      {:ok, view, html} = live(conn, device_path(fixture, :console))
 
       refute html =~ "Howdy"
 
@@ -119,5 +107,9 @@ defmodule NervesHubWWWWeb.DeviceLiveConsoleTest do
 
       assert render(view) =~ "wat it do?"
     end
+  end
+
+  def device_path(%{device: device, org: org, product: product}, type) do
+    Routes.device_path(Endpoint, type, org.name, product.name, device.identifier)
   end
 end
