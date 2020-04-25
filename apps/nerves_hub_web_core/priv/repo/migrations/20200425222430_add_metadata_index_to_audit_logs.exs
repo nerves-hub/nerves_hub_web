b9d654065bb0ee7adfb0599924eca1a5c9c8d9cb
Justin Schneck <jschneck@mac.com>
2020-04-25 18:37:10 -0400

    Add an index to audit_logs params->firmware_uuid

---
+++ apps/nerves_hub_web_core/priv/repo/migrations/20200425222430_add_metadata_index_to_audit_logs.exs
@@ -0,0 +1,11 @@
+defmodule NervesHubWebCore.Repo.Migrations.AddMetadataIndexToAuditLogs do
+  use Ecto.Migration
+
+  def up do
+    execute("CREATE INDEX audit_param_firmware_uuid ON audit_logs((params->'firmware_uuid'));")
+  end
+
+  def down do
+    execute("DROP INDEX audit_param_firmware_uuid")
+  end
+end
