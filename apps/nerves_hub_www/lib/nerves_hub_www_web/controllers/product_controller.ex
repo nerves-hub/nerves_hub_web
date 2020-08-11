0ae9af6e4487a37b359bd0142fe2184742a4784f
cory jewell <cory@verypossible.com>
2020-08-11 15:50:56 -0400

    No permissions to change product name

--- apps/nerves_hub_www/lib/nerves_hub_www_web/controllers/product_controller.ex
+++ apps/nerves_hub_www/lib/nerves_hub_www_web/controllers/product_controller.ex
@@ -55,7 +55,7 @@ defmodule NervesHubWWWWeb.ProductController do
       {:ok, product} ->
         conn
         |> put_flash(:info, "Product updated successfully.")
-        |> redirect(to: Routes.product_path(conn, :show, org.name, product.name))
+        |> redirect(to: Routes.device_path(conn, :index, org.name, product.name))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", product: product, changeset: changeset)
