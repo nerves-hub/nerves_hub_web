3f5b4f3568bb6598dba6a579fa379d026d36e23f
cory jewell <cory@verypossible.com>
2020-09-04 14:23:10 -0400

    Remove unused javascript date formatting

--- apps/nerves_hub_www/assets/js/app.js
+++ apps/nerves_hub_www/assets/js/app.js
@@ -7,7 +7,6 @@ import { Socket } from 'phoenix'
 import LiveSocket from 'phoenix_live_view'
 import IEx from './console'
 
-let dates = require('./dates')
 const iex = new IEx()
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
@@ -18,10 +17,6 @@ let liveSocket = new LiveSocket('/live', Socket, {
 let socket = new Socket('/socket', { params: { token: window.userToken } })
 
 liveSocket.connect()
-
-document.querySelectorAll('.date-time').forEach(d => {
-  d.innerHTML = dates.formatDateTime(d.innerHTML)
-})
 
 $(function() {
   $('.custom-upload-input').on('change', function() {
