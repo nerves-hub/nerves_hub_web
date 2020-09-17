b85405ff84f65de572e0c85ca0ecee098d286095
Justin Schneck <jschneck@mac.com>
2020-09-16 21:18:12 -0400

    Fix compiler warnings

--- test/support/mocks.ex
+++ test/support/mocks.ex
@@ -1,2 +1,4 @@
+Code.compiler_options(ignore_module_conflict: true)
+
 Mox.defmock(NervesHubWebCore.PatcherMock, for: NervesHubWebCore.Firmwares.Patcher)
 Mox.defmock(NervesHubWebCore.UploadMock, for: NervesHubWebCore.Firmwares.Upload)
