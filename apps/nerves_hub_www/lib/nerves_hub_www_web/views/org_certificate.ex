184401aff41abc6d081ee09bc25da8139e5b4d9e
Connor Rigby <connorr@hey.com>
2020-11-24 13:05:44 -0800

    Show certificate serial number in hex to match openssl

--- apps/nerves_hub_www/lib/nerves_hub_www_web/views/org_certificate.ex
+++ apps/nerves_hub_www/lib/nerves_hub_www_web/views/org_certificate.ex
@@ -2,4 +2,12 @@ defmodule NervesHubWWWWeb.OrgCertificateView do
   use NervesHubWWWWeb, :view
 
   alias NervesHubWWWWeb.LayoutView.DateTimeFormat, as: DateTimeFormat
+
+  def format_serial(big_long_integer) when is_integer(big_long_integer) do
+    big_long_integer
+    |> Integer.to_string(16)
+    |> to_charlist()
+    |> Enum.chunk_every(2)
+    |> Enum.join(":")
+  end
 end
