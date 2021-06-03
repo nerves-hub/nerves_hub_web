326f3514fd2f9255af4ab766d0137995c023edd5
Ryota Kinukawa <pojiro.jp@gmail.com>
2021-06-04 08:48:32 +0900

    Fix allow_signups runtime config is not available
    The reason why this issue occurs is due to the fact that
    the value of allow_signups is assigned in the init function of plug.
    
    This is mentioned in the Plug documentation.
    > Note that init/1 may be called during compilation and as such
    > it must not return pids, ports or values that are specific to the runtime.

--- apps/nerves_hub_web_core/lib/nerves_hub_web_core/plugs/allow_uninvited_signups.ex
+++ apps/nerves_hub_web_core/lib/nerves_hub_web_core/plugs/allow_uninvited_signups.ex
@@ -6,30 +6,34 @@ defmodule NervesHubWebCore.Plugs.AllowUninvitedSignups do
 
   alias Phoenix.Controller
 
-  def init(opts) do
-    Keyword.put(opts, :allow_signups, allow_signups())
-  end
-
-  def call(conn, allow_signups: true), do: conn
+  def init(opts), do: opts
 
   def call(%{private: %{phoenix_router: APIRouter}} = conn, _opts) do
-    conn
-    |> put_resp_header("content-type", "application/json")
-    |> send_resp(
-      403,
-      Jason.encode!(%{status: "Public signups disabled. Invite required to signup"})
-    )
-    |> halt()
+    if allow_signups() do
+      conn
+    else
+      conn
+      |> put_resp_header("content-type", "application/json")
+      |> send_resp(
+        403,
+        Jason.encode!(%{status: "Public signups disabled. Invite required to signup"})
+      )
+      |> halt()
+    end
   end
 
   def call(%{private: %{phoenix_router: WebRouter}} = conn, _opts) do
-    conn
-    |> Controller.put_flash(:error, "Sorry signups are not enabled at this time.")
-    |> Controller.redirect(to: "/")
-    |> halt()
+    if allow_signups() do
+      conn
+    else
+      conn
+      |> Controller.put_flash(:error, "Sorry signups are not enabled at this time.")
+      |> Controller.redirect(to: "/")
+      |> halt()
+    end
   end
 
-  defp allow_signups do
+  defp allow_signups() do
     Application.get_env(:nerves_hub_web_core, :allow_signups)
   end
 end
--- apps/nerves_hub_web_core/test/nerves_hub_web_core/plugs/allow_uninvited_signups_test.exs
+++ apps/nerves_hub_web_core/test/nerves_hub_web_core/plugs/allow_uninvited_signups_test.exs
@@ -16,7 +16,8 @@ defmodule NervesHubWebCore.Plugs.AllowUninvitedSignupsTest do
     end
 
     test "user is redirected when signups are disabled", %{conn: conn} do
-      conn = NervesHubWebCore.Plugs.AllowUninvitedSignups.call(conn, allow_signups: false)
+      Application.put_env(:nerves_hub_web_core, :allow_signups, false)
+      conn = NervesHubWebCore.Plugs.AllowUninvitedSignups.call(conn, [])
 
       assert redirected_to(conn) == "/"
 
@@ -26,7 +27,8 @@ defmodule NervesHubWebCore.Plugs.AllowUninvitedSignupsTest do
     end
 
     test "user is not redirected when signups are enabled", %{conn: conn} do
-      conn_through = NervesHubWebCore.Plugs.AllowUninvitedSignups.call(conn, allow_signups: true)
+      Application.put_env(:nerves_hub_web_core, :allow_signups, true)
+      conn_through = NervesHubWebCore.Plugs.AllowUninvitedSignups.call(conn, [])
 
       assert conn_through.status != 302
       assert conn_through == conn
@@ -44,7 +46,8 @@ defmodule NervesHubWebCore.Plugs.AllowUninvitedSignupsTest do
     end
 
     test "fails registration when signups are disabled", %{conn: conn} do
-      conn = NervesHubWebCore.Plugs.AllowUninvitedSignups.call(conn, allow_signups: false)
+      Application.put_env(:nerves_hub_web_core, :allow_signups, false)
+      conn = NervesHubWebCore.Plugs.AllowUninvitedSignups.call(conn, [])
 
       assert json_response(conn, 403) == %{
                "status" => "Public signups disabled. Invite required to signup"
@@ -52,7 +55,9 @@ defmodule NervesHubWebCore.Plugs.AllowUninvitedSignupsTest do
     end
 
     test "passes conn when signups are allowed", %{conn: conn} do
-      conn_through = NervesHubWebCore.Plugs.AllowUninvitedSignups.call(conn, allow_signups: true)
+      Application.put_env(:nerves_hub_web_core, :allow_signups, true)
+      conn_through = NervesHubWebCore.Plugs.AllowUninvitedSignups.call(conn, [])
+
       assert conn_through == conn
     end
   end
