a2d3f745d339c925ed40a6bfccc1af985250a1cf
Jon Carstens <jjcarstens@me.com>
2021-07-21 07:49:53 -0600

    js security audit updates

--- apps/nerves_hub_www/assets/webpack.config.js
+++ apps/nerves_hub_www/assets/webpack.config.js
@@ -2,14 +2,15 @@ const path = require('path');
 const glob = require('glob');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => ({
+  cache: true,
   optimization: {
     minimizer: [
-      new TerserPlugin({ cache: true, parallel: true, sourceMap: false }),
-      new OptimizeCSSAssetsPlugin({})
+      new TerserPlugin({ parallel: true }),
+      new CssMinimizerPlugin({})
     ]
   },
   entry: {
@@ -62,6 +63,6 @@ module.exports = (env, options) => ({
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-    new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
+    new CopyWebpackPlugin({patterns: [{ from: 'static/', to: '../' }]})
   ]
 });
