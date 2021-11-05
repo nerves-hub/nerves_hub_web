fa2cef1a4a3619e33088d3c700b4e75722d12dd9
Jon Carstens <jjcarstens@me.com>
2021-11-05 10:06:09 -0600

    Remove support for device HTTP API requests
    This changes the focus of device -> server communication to rely on
    websockets and channels. Also removes all supporting code for an HTTP
    API interface that are no longer needed

--- apps/nerves_hub_device/lib/nerves_hub_device_web/endpoint.ex
+++ apps/nerves_hub_device/lib/nerves_hub_device_web/endpoint.ex
@@ -9,50 +9,13 @@ defmodule NervesHubDeviceWeb.Endpoint do
     ]
   )
 
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phoenix.digest
-  # when deploying your static files in production.
-  plug(
-    Plug.Static,
-    at: "/",
-    from: :nerves_hub_device,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
-  )
-
-  plug(Plug.RequestId)
-  plug(Plug.Logger)
-
-  plug(
-    Plug.Parsers,
-    parsers: [:urlencoded, :multipart, :json],
-    pass: ["*/*"],
-    length: 160_000_000,
-    json_decoder: Jason
-  )
-
-  plug(Plug.MethodOverride)
-  plug(Plug.Head)
-
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  plug(
-    Plug.Session,
-    store: :cookie,
-    key: "_nerves_hub_key",
-    signing_salt: "1CPjriVa"
-  )
-
-  plug(NervesHubDeviceWeb.Router)
-
   @doc """
   Callback invoked for dynamically configuring the endpoint.
 
   It receives the endpoint configuration and checks if
   configuration should be loaded from the system environment.
   """
+  @impl Phoenix.Endpoint
   def init(_key, config) do
     config = verify_fun(config)
 
