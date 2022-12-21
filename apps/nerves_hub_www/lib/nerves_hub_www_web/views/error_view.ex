b8f69b5f67a6d60465ef6aaaab5cf197a2d16f9f
Melvin <melvin.cedeno@smartrent.com>
2022-12-20 18:07:26 -0700

    adds custom 404 page

--- apps/nerves_hub_www/lib/nerves_hub_www_web/views/error_view.ex
+++ apps/nerves_hub_www/lib/nerves_hub_www_web/views/error_view.ex
@@ -1,10 +1,6 @@
 defmodule NervesHubWWWWeb.ErrorView do
   use NervesHubWWWWeb, :view
 
-  def render("404.html", _assigns) do
-    "Page not found"
-  end
-
   def render("500.html", _assigns) do
     "Internal server error"
   end
--- apps/nerves_hub_www/test/nerves_hub_www_web/views/error_view_test.exs
+++ apps/nerves_hub_www/test/nerves_hub_www_web/views/error_view_test.exs
@@ -4,10 +4,6 @@ defmodule NervesHubWWWWeb.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
-  test "renders 404.html" do
-    assert render_to_string(NervesHubWWWWeb.ErrorView, "404.html", []) == "Page not found"
-  end
-
   test "render 500.html" do
     assert render_to_string(NervesHubWWWWeb.ErrorView, "500.html", []) == "Internal server error"
   end
