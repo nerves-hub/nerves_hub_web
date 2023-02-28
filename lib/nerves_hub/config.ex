43302e9b70fdeef6fbaf32f0937085b3d0a1ec41
Eric Oestrich <eric@oestrich.org>
2023-02-28 15:35:46 -0500

    LiveView signing salt is optional and will be used for web only (#105)

--- lib/nerves_hub/config.ex
+++ lib/nerves_hub/config.ex
@@ -12,10 +12,10 @@ defmodule NervesHub.Config do
   config :endpoint,
          env([
            {:http_host, "HOST"},
-           {:live_view_signing_salt, "LIVE_VIEW_SIGNING_SALT"},
+           {:live_view_signing_salt, "LIVE_VIEW_SIGNING_SALT", optional: true},
            {:secret_key_base, "SECRET_KEY_BASE"},
            {:url_host, "HOST"},
-           {:url_port, "URL_PORT", map: &String.to_integer/1},
-           {:url_scheme, "URL_SCHEME", default: "http"}
+           {:url_port, "URL_PORT", default: "443", map: &String.to_integer/1},
+           {:url_scheme, "URL_SCHEME", default: "https"}
          ])
 end
