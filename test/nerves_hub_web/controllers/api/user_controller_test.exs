65dea9eff7f87b6f09fe7eeb90c1b5486607baf9
Eric Oestrich <eric@oestrich.org>
2023-03-23 17:16:16 -0400

    Remove `:ca_integration` tests
    They are excluded from `mix test` and some amount of the code behind it
    has been removed (due to removing NervesHubCA integration.)

--- test/nerves_hub_web/controllers/api/user_controller_test.exs
+++ test/nerves_hub_web/controllers/api/user_controller_test.exs
@@ -2,8 +2,6 @@ defmodule NervesHubWeb.API.UserControllerTest do
   use NervesHubWeb.APIConnCase, async: true
 
   alias NervesHub.Fixtures
-  alias NervesHub.Certificate
-  alias NervesHub.Accounts
 
   test "me", %{conn: conn, user: user} do
     conn = get(conn, Routes.user_path(conn, :me))
@@ -100,34 +98,5 @@ defmodule NervesHubWeb.API.UserControllerTest do
              "username" => user.username,
              "email" => user.email
            }
-  end
-
-  @tag :ca_integration
-  test "sign new registration certificates" do
-    subject = "/O=NervesHub/CN=username"
-    key = X509.PrivateKey.new_ec(:secp256r1)
-
-    csr =
-      X509.CSR.new(key, subject)
-      |> X509.CSR.to_pem()
-      |> Base.encode64()
-
-    params =
-      Fixtures.user_fixture(name: "username")
-      |> Map.take([:email, :password])
-      |> Map.put(:csr, csr)
-      |> Map.put(:description, "test-machine")
-
-    conn = build_conn()
-
-    conn = post(conn, Routes.user_path(conn, :sign), params)
-    resp_data = json_response(conn, 200)["data"]
-    assert %{"cert" => cert} = resp_data
-
-    cert = X509.Certificate.from_pem!(cert)
-    serial = Certificate.get_serial_number(cert)
-
-    user = Accounts.get_user_by_certificate_serial(serial)
-    assert user.email == params.email
   end
 end
