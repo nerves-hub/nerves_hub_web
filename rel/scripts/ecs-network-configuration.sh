aa58ba4b331a33bd7d8117c4d267d1dd5496d5f3
Eric Oestrich <eric@oestrich.org>
2023-03-29 18:05:56 -0400

    Add back ecs-network-configuration.sh

---
+++ rel/scripts/ecs-network-configuration.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+VPC=$1
+SG=$2
+
+VPCS=$(aws ec2 describe-vpcs --output json | jq ".Vpcs")
+VPC=$(echo $VPCS | jq --arg vpc $VPC '.[] | select((.Tags[]|select(.Key=="Name")|.Value) == $vpc) | .VpcId')
+SUBNETS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC" --output json | jq -c '[.Subnets[] | select((.Tags[]|select(.Key=="Name")|.Value) | contains("private")) | .SubnetId]')
+SECURITY_GROUPS=$(aws ec2 describe-security-groups --filters=Name=tag:Name,Values=$SG | jq -c '[.SecurityGroups[] | .GroupId ]')
+
+echo "awsvpcConfiguration={subnets=$SUBNETS,securityGroups=$SECURITY_GROUPS,assignPublicIp=DISABLED}"
