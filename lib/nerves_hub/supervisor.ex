c37c21a72beb1d9675d2be85d7d32e41951a7e6b
Eric Oestrich <eric@oestrich.org>
2023-04-30 15:57:11 -0400

    Remove NervesHub.Workers
    OrgMetrics is broken since the function doesn't exist, and I'm not sure
    we need the S3 ingress parser either.

--- lib/nerves_hub/supervisor.ex
+++ lib/nerves_hub/supervisor.ex
@@ -40,68 +40,11 @@ defmodule NervesHub.Supervisor do
     Supervisor.init(children, opts)
   end
 
-  def start_phase(:start_workers, _start_type, _phase_args) do
-    if Application.get_env(:nerves_hub_www, :enable_workers) do
-      # Look for all workers using the NervesHub.Worker behaviour
-      # and attempt schedule_next/0 which is idempotent. So if a job
-      # record already exists for the next scheduled time, then it
-      # will not be duplicated.
-      #
-      # Jobs run recursively so we just need to ensure one is scheduled
-      # at start-up. When the job runs, it will schedule its next run time.
-      for module <- worker_modules() do
-        worker_startup = fn ->
-          # Schedule next 2 jobs
-          job = module.schedule_next!()
-
-          DateTime.diff(job.scheduled_at, DateTime.utc_now())
-          |> abs()
-          # right on the scheduled time
-          |> Kernel.+(1)
-          |> module.schedule_next!()
-        end
-
-        Task.Supervisor.start_child(
-          NervesHub.TaskSupervisor,
-          worker_startup,
-          restart: :transient
-        )
-      end
-
-      Logger.info("Workers started")
-    else
-      Logger.info("Workers disabled - skipping job scheduling")
-    end
-  end
-
   defp configure_oban() do
-    config = Application.get_env(:nerves_hub_www, Oban, [])
-
-    case Keyword.get(config, :queues, []) do
-      false ->
-        # queues need to be ignored if set to false
-        config
-
-      config_queues ->
-        # Ensure worker defined queues are added
-        worker_queues =
-          for worker <- worker_modules() do
-            {worker.config[:queue], worker.config[:concurrent_jobs]}
-          end
-
-        Keyword.put(config, :queues, Keyword.merge(config_queues, worker_queues))
-    end
+    Application.get_env(:nerves_hub_www, Oban, [])
   end
 
-  defp worker_modules() do
-    for {module, _} <- :code.all_loaded(),
-        behaviors = Keyword.get_values(module.module_info(:attributes), :behaviour),
-        [NervesHub.Worker] in behaviors do
-      module
-    end
-  end
-
-  defp datadog_children do
+  defp datadog_children() do
     opts = [
       host: Application.get_env(:nerves_hub_www, :datadog_host, "localhost"),
       port: to_integer(Application.get_env(:nerves_hub_www, :datadog_port)),
