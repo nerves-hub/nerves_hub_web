7f77ab283701eae67d5a7f4d7584ca08a9fc7322
Tim Cooper <tim@timcorp.io>
2023-10-26 11:57:30 +1000

    Remove unused api_endpoint from vapor config
    This configuration is not referenced anywhere in the codebase.

--- lib/nerves_hub/config.ex
+++ lib/nerves_hub/config.ex
@@ -9,14 +9,6 @@ defmodule NervesHub.Config do
            {:pool_size, "POOL_SIZE", default: 5, map: &String.to_integer/1}
          ])
 
-  config :api_endpoint,
-         env([
-           {:secret_key_base, "SECRET_KEY_BASE"},
-           {:url_host, "HOST"},
-           {:url_port, "URL_PORT", default: "443", map: &String.to_integer/1},
-           {:url_scheme, "URL_SCHEME", default: "https"}
-         ])
-
   config :web_endpoint,
          env([
            {:live_view_signing_salt, "LIVE_VIEW_SIGNING_SALT", required: false},
