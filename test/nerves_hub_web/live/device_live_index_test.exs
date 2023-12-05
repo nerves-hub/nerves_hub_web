0fa5b3c53d62dc43c5fdc6bd9bb7b055d09456fc
Eric Oestrich <eric@oestrich.org>
2023-12-05 12:59:14 -0500

    Remove flaky test around pagination in the device index

--- test/nerves_hub_web/live/device_live_index_test.exs
+++ test/nerves_hub_web/live/device_live_index_test.exs
@@ -47,42 +47,6 @@ defmodule NervesHubWeb.DeviceLiveIndexTest do
       refute render_change(view, "update-filters", %{"tag" => "doesntmatter"}) =~
                device2.identifier
     end
-
-    test "paginates devices", %{conn: conn, fixture: fixture} do
-      %{device: device, firmware: firmware, org: org, product: product} = fixture
-
-      device2 = Fixtures.device_fixture(org, product, firmware)
-      device3 = Fixtures.device_fixture(org, product, firmware)
-
-      {:ok, view, _html} = live(conn, device_index_path(fixture))
-      html = render_change(view, "set-paginate-opts", %{"page-size" => "1"})
-
-      assert html =~ device.identifier
-      refute html =~ device2.identifier
-
-      html = render_change(view, "paginate", %{"page" => "2"})
-      assert html =~ device2.identifier
-      refute html =~ device.identifier
-
-      html = render_change(view, "set-paginate-opts", %{"page-size" => "1"})
-      assert html =~ device.identifier
-      refute html =~ device2.identifier
-
-      html = render_change(view, "paginate", %{"page" => "3"})
-      refute html =~ device.identifier
-      refute html =~ device2.identifier
-      assert html =~ device3.identifier
-
-      html = render_change(view, "set-paginate-opts", %{"page-size" => "2"})
-      assert html =~ device.identifier
-      assert html =~ device2.identifier
-      refute html =~ device3.identifier
-
-      html = render_change(view, "paginate", %{"page" => "2"})
-      refute html =~ device.identifier
-      refute html =~ device2.identifier
-      assert html =~ device3.identifier
-    end
   end
 
   def device_index_path(%{org: org, product: product}) do
