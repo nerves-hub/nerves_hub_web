878fb14d5e594e1d364cc8433fe363f9d3339a70
Josh Kalderimis <josh.kalderimis@gmail.com>
2023-12-12 11:06:31 +1300

    some changes to the algorithm by @jjcarstens

--- lib/nerves_hub_web/channels/device_socket_shared_secret_auth.ex
+++ lib/nerves_hub_web/channels/device_socket_shared_secret_auth.ex
@@ -9,27 +9,15 @@ defmodule NervesHubWeb.DeviceSocketSharedSecretAuth do
   channel("console", NervesHubWeb.ConsoleChannel)
   channel("device", NervesHubWeb.DeviceChannel)
 
-  @salt_headers [
-    "x-nh-digest",
-    "x-nh-iterations",
-    "x-nh-length",
-    "x-nh-key",
-    "x-nh-time"
-  ]
-
   # Default 1 min max age for the signature
   @default_max_age 60
 
   def connect(_params, socket, %{x_headers: headers}) do
-    parsed_headers = parse_headers(headers)
-    verification_options = verification_options(parsed_headers)
-    salt = expected_salt(headers)
-
     with {:ok, true} <- Application.fetch_env(:nerves_hub, __MODULE__)[:enabled],
-         {:ok, key} <- Keyword.fetch(parsed_headers, :key),
+         {:ok, key, salt, verification_opts} <- decode_from_headers(headers),
          {:ok, auth} <- Products.get_shared_secret_auth(key),
-         {:ok, signature} <- Keyword.fetch(parsed_headers, :signature),
-         {:ok, identifier} <- Crypto.verify(auth.secret, salt, signature, verification_options),
+         {:ok, signature} <- Map.fetch(headers, "x-nh-signature"),
+         {:ok, identifier} <- Crypto.verify(auth.secret, salt, signature, verification_opts),
          {:ok, device} <- Devices.get_or_create_device(auth, identifier) do
       socket =
         socket
@@ -45,53 +33,34 @@ defmodule NervesHubWeb.DeviceSocketSharedSecretAuth do
   def id(%{assigns: %{device: device}}), do: "device_socket:#{device.id}"
   def id(_socket), do: nil
 
-  defp parse_headers(headers) do
-    for {k, v} <- headers do
-      case String.downcase(k) do
-        "x-nh-digest" ->
-          "NH1-HMAC-" <> digest_str = v
-          {:key_digest, String.to_existing_atom(String.downcase(digest_str))}
+  defp decode_from_headers(%{"x-nh-alg" => "NH1-HMAC-" <> alg} = headers) do
+    with [digest_str, iter_str, klen_str] <- String.split(alg, "-"),
+         digest <- String.to_existing_atom(String.downcase(digest_str)),
+         {iterations, ""} <- Integer.parse(iter_str),
+         {key_length, ""} <- Integer.parse(klen_str),
+         {signed_at, ""} <- Integer.parse(headers["x-nh-time"]),
+         {:ok, key} <- Map.fetch(headers, "x-nh-key") do
+      expected_salt = """
+      NH1:device-socket:shared-secret:connect
 
-        "x-nh-iterations" ->
-          {:key_iterations, String.to_integer(v)}
+      x-nh-alg=NH1-HMAC-#{alg}
+      x-nh-key=#{key}
+      x-nh-time=#{signed_at}
+      """
 
-        "x-nh-length" ->
-          {:key_length, String.to_integer(v)}
+      opts = [
+        key_length: key_length,
+        key_iterations: iterations,
+        key_digest: digest,
+        signed_at: signed_at,
+        max_age: @default_max_age
+      ]
 
-        "x-nh-key" ->
-          {:key, v}
-
-        "x-nh-signature" ->
-          {:signature, v}
-
-        "x-nh-time" ->
-          {:signed_at, String.to_integer(v)}
-
-        _ ->
-          # Skip unknown x headers.
-          # It's not uncommon for x headers to be added by Load balancers
-          nil
-      end
+      {:ok, key, expected_salt, opts}
     end
   end
 
-  defp verification_options(parsed_data) do
-    Keyword.take(parsed_data, [:key_digest, :key_iterations, :key_length, :signed_at])
-    |> Keyword.put(:max_age, @default_max_age)
-  end
-
-  defp expected_salt(headers) do
-    salt_headers =
-      Enum.filter(headers, &(elem(&1, 0) in @salt_headers))
-      |> Enum.sort_by(fn {k, _} -> k end)
-      |> Enum.map_join("\n", fn {k, v} -> "#{k}=#{v}" end)
-
-    """
-    NH1:device:token:connect
-
-    #{salt_headers}
-    """
-  end
+  defp decode_from_headers(_headers), do: :error
 
   defp generate_reference_id() do
     Base.encode32(:crypto.strong_rand_bytes(2), padding: false)
