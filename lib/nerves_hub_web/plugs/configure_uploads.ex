63d8186c266007c710f1464ef10998893ad9da65
Josh Kalderimis <josh.kalderimis@gmail.com>
2023-12-24 18:40:37 +1300

    fix an application env with `ConfigureUploads`

--- lib/nerves_hub_web/plugs/configure_uploads.ex
+++ lib/nerves_hub_web/plugs/configure_uploads.ex
@@ -4,12 +4,16 @@ defmodule NervesHubWeb.Plugs.ConfigureUploads do
   def init(_opts), do: []
 
   def call(conn, _opts) do
-    if System.get_env("FIRMWARE_UPLOAD_BACKEND") == "local" do
+    if local_uploads?() do
       conn
       |> NervesHubWeb.Plugs.FileUpload.call([])
       |> NervesHubWeb.Plugs.StaticUploads.call([])
     else
       conn
     end
+  end
+
+  defp local_uploads?() do
+    Application.get_env(:nerves_hub, :firmware_upload) == NervesHub.Firmwares.Upload.File
   end
 end
