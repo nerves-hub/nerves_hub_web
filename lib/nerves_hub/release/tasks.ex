2b1def53ea0fe610bfb61b0fb02b6a960136f9ba
Jon Carstens <jon.carstens@smartrent.com>
2024-01-04 11:22:16 -0700

    Fix release migrations
    Previously, release migrations were attempting to be handled in `Enum.each` without
    any return checks. This means failures could be returned but were unhandled and
    false positive of success could be relayed.
    
    Instead, this updates to match the Ecto and Phoenix recommendations for dealing
    with migrations in a release. See also:
    
    * https://hexdocs.pm/ecto_sql/3.11.1/Ecto.Migrator.html#module-example-running-migrations-in-a-release
    * https://hexdocs.pm/phoenix/releases.html#ecto-migrations-and-custom-commands

--- lib/nerves_hub/release/tasks.ex
+++ lib/nerves_hub/release/tasks.ex
@@ -1,67 +1,46 @@
 defmodule NervesHub.Release.Tasks do
   alias Ecto.Migrator
 
-  @otp_app :nerves_hub
-  @start_apps [:logger, :ssl, :postgrex, :ecto_sql]
+  @app :nerves_hub
+  @migrate_opts [
+    pool_size: 10,
+    start_apps_before_migration: [:logger, :ssl, :postgrex, :ecto_sql]
+  ]
 
   def migrate() do
-    init(@otp_app, @start_apps)
-    run_migrations_for(@otp_app)
-    stop()
+    Application.load(@app)
+
+    for repo <- Application.fetch_env!(@app, :ecto_repos) do
+      {:ok, _, _} = Migrator.with_repo(repo, &Migrator.run(&1, :up, all: true), @migrate_opts)
+    end
   end
 
   def migrate_and_seed() do
-    init(@otp_app, @start_apps)
-
-    run_migrations_for(@otp_app)
-    run_seed_script("#{seed_path(@otp_app)}/seeds.exs")
-
-    stop()
+    _ = migrate()
+    seed()
   end
 
-  defp init(app, start_apps) do
-    IO.puts("Loading nerves_hub_www app for migrations...")
-    Application.load(app)
-
-    IO.puts("Starting dependencies...")
-    Enum.each(start_apps, &Application.ensure_all_started/1)
-
-    IO.puts("Starting repos...")
-
-    app
-    |> Application.get_env(:ecto_repos, [])
-    |> Enum.each(& &1.start_link(pool_size: 10))
+  def rollback(repo \\ NervesHub.Repo, version) do
+    Application.load(@app)
+    {:ok, _, _} = Migrator.with_repo(repo, &Migrator.run(&1, :down, to: version), @migrate_opts)
   end
 
-  defp stop do
-    IO.puts("Success!")
-    :init.stop()
-  end
+  def seed() do
+    Application.load(@app)
 
-  defp run_migrations_for(app) do
-    IO.puts("Running migrations for #{app}")
-
-    app
-    |> Application.get_env(:ecto_repos, [])
-    |> Enum.each(&Migrator.run(&1, migrations_path(app), :up, all: true))
-  end
-
-  def run_seed_script(seed_script) do
-    IO.puts("Running seed script #{seed_script}...")
-    Code.eval_file(seed_script)
-  end
-
-  defp migrations_path(app), do: priv_dir(app, ["repo", "migrations"])
-
-  defp seed_path(app), do: priv_dir(app, ["repo"])
-
-  defp priv_dir(app, path) when is_list(path) do
-    case :code.priv_dir(app) do
-      priv_path when is_list(priv_path) or is_binary(priv_path) ->
-        Path.join([priv_path] ++ path)
-
-      {:error, :bad_name} ->
-        raise ArgumentError, "unknown application: #{inspect(app)}"
+    with priv_path when is_list(priv_path) or is_binary(priv_path) <- :code.priv_dir(@app),
+         seed_script = Path.join(priv_path, "repo/seeds.exs"),
+         true <- File.exists?(seed_script),
+         {:ok, _, _} <-
+           Migrator.with_repo(
+             NervesHub.Repo,
+             fn _ -> Code.eval_file(seed_script) end,
+             @migrate_opts
+           ) do
+      :ok
+    else
+      err ->
+        raise "Failed to run seed script: #{inspect(err)}"
     end
   end
 end
