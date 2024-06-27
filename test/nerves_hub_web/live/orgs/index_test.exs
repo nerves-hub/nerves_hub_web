e76f08a4c3f8da27bd185e8dc16fa9e5e35fe6ee
Josh Kalderimis <josh.kalderimis@gmail.com>
2024-06-27 22:39:11 +1200

    fixes to tests

--- test/nerves_hub_web/live/orgs/index_test.exs
+++ test/nerves_hub_web/live/orgs/index_test.exs
@@ -9,16 +9,14 @@ defmodule NervesHubWeb.Live.Orgs.IndexTest do
       |> init_test_session(%{"auth_user_id" => user.id})
       |> visit("/orgs")
       |> assert_has("h3", text: "You aren't a member of any organizations.")
-      |> PhoenixTest.open_browser()
     end
   end
 
   describe "has orgs memberships" do
-    test "all orgs listed", %{conn: conn, org: org, user: user} do
+    test "all orgs listed", %{conn: conn, org: org} do
       conn
       |> visit("/orgs")
       |> assert_has("h1", text: "My Organizations")
-      |> assert_has("h3", text: user.username)
       |> assert_has("h3", text: org.name)
     end
   end
