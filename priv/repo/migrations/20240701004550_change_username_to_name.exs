79246c5f655ceb4808e72c4807b22bff94617a6b
Josh Kalderimis <josh.kalderimis@gmail.com>
2024-07-04 13:48:29 +1200

    removed duplicated migration

--- priv/repo/migrations/20240701004550_change_username_to_name.exs
+++
@@ -1,9 +0,0 @@
-defmodule NervesHub.Repo.Migrations.ChangeUsernameToName do
-  use Ecto.Migration
-
-  def up do
-    drop index("users", [:username], name: "users_username_index")
-
-    rename table("users"), :username, to: :name
-  end
-end
