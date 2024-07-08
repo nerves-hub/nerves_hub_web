d5411a93f9e5a3c3d502c4285f5897ff7addfdef
Lars Wikman <lars@underjord.io>
2024-07-08 21:30:06 +0200

    Fix tests for device_channel

--- test/nerves_hub_web/channels/device_channel_test.exs
+++ test/nerves_hub_web/channels/device_channel_test.exs
@@ -19,6 +19,7 @@ defmodule NervesHubWeb.DeviceChannelTest do
 
     {:ok, _, socket} = subscribe_and_join(socket, DeviceChannel, "device")
     assert socket
+    assert_push("check_health", %{})
   end
 
   test "presence connection information" do
