a1ec69e79a51e544b3f48396f664397734fd731a
Josh Kalderimis <josh.kalderimis@gmail.com>
2024-07-13 10:43:44 +1200

    move the sentry filter to its own file

--- lib/nerves_hub_web/errors.ex
+++ lib/nerves_hub_web/errors.ex
@@ -1,19 +1,3 @@
 defmodule NervesHubWeb.NotFoundError do
   defexception message: "not found", plug_status: 404
 end
-
-defmodule NervesHubWeb.SentryEventFilter do
-  def filter_non_500(%Sentry.Event{original_exception: exception} = event) do
-    cond do
-      Plug.Exception.status(exception) < 500 ->
-        false
-
-      # Fall back to the default event filter.
-      Sentry.DefaultEventFilter.exclude_exception?(exception, event.source) ->
-        false
-
-      true ->
-        event
-    end
-  end
-end
