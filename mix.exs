2cdad8cae5d15355217a6c3544f67be7ae05efe3
Josh Kalderimis <josh.kalderimis@gmail.com>
2024-07-18 10:55:18 +1200

    The `req` dependency is no longer used

--- mix.exs
+++ mix.exs
@@ -96,7 +96,6 @@ defmodule NervesHub.MixProject do
       {:phoenix_test, "~> 0.3.0", only: :test, runtime: false},
       {:plug, "~> 1.7"},
       {:postgrex, "~> 0.14"},
-      {:req, "~> 0.5.0"},
       {:scrivener_ecto, "~> 2.7"},
       {:scrivener_html, git: "https://github.com/nerves-hub/scrivener_html", branch: "phx-1.5"},
       {:sentry, "~> 10.0"},
