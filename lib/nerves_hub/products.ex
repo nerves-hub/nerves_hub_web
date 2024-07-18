7a5f4c2a7e7425a13f0858b2d3271634fb13cb7b
Josh Kalderimis <josh.kalderimis@gmail.com>
2024-07-18 21:35:03 +1200

    Add specs for all functions in the Products context

--- lib/nerves_hub/products.ex
+++ lib/nerves_hub/products.ex
@@ -17,6 +17,7 @@ defmodule NervesHub.Products do
 
   def __csv_header__, do: @csv_header
 
+  @spec get_products_by_user_and_org(User.t(), Org.t()) :: [Product.t()]
   def get_products_by_user_and_org(%User{id: user_id}, %Org{id: org_id}) do
     query =
       from(
@@ -48,12 +49,14 @@ defmodule NervesHub.Products do
       ** (Ecto.NoResultsError)
 
   """
+  @spec get_product!(pos_integer()) :: Product.t()
   def get_product!(id) do
     Product
     |> Repo.exclude_deleted()
     |> Repo.get!(id)
   end
 
+  @spec get_product(pos_integer()) :: {:ok, Product.t()} | {:error, :not_found}
   def get_product(id) do
     Product
     |> Repo.exclude_deleted()
@@ -64,11 +67,14 @@ defmodule NervesHub.Products do
     end
   end
 
+  @spec get_product_by_org_id_and_name!(pos_integer(), String.t()) :: Product.t()
   def get_product_by_org_id_and_name!(org_id, name) do
     get_product_by_org_id_and_name_query(org_id, name)
     |> Repo.one!()
   end
 
+  @spec get_product_by_org_id_and_name(pos_integer(), String.t()) ::
+          {:ok, Product.t()} | {:error, :not_found}
   def get_product_by_org_id_and_name(org_id, name) do
     get_product_by_org_id_and_name_query(org_id, name)
     |> Repo.one()
@@ -87,6 +93,7 @@ defmodule NervesHub.Products do
   @doc """
   Creates a product.
   """
+  @spec create_product(map()) :: {:ok, Product.t()} | {:error, Ecto.Changeset.t()}
   def create_product(params) do
     multi =
       Multi.new()
@@ -113,6 +120,7 @@ defmodule NervesHub.Products do
       {:error, %Ecto.Changeset{}}
 
   """
+  @spec update_product(Product.t(), map()) :: {:ok, Product.t()} | {:error, Ecto.Changeset.t()}
   def update_product(%Product{} = product, attrs) do
     product
     |> Product.update_changeset(attrs)
@@ -131,6 +139,7 @@ defmodule NervesHub.Products do
       {:error, %Ecto.Changeset{}}
 
   """
+  @spec delete_product(Product.t()) :: {:ok, Product.t()} | {:error, Ecto.Changeset.t()}
   def delete_product(%Product{} = product) do
     product
     |> Product.delete_changeset()
@@ -146,10 +155,13 @@ defmodule NervesHub.Products do
       %Ecto.Changeset{source: %Product{}}
 
   """
+  @spec change_product(Product.t()) :: Ecto.Changeset.t()
   def change_product(%Product{} = product) do
     Product.changeset(product, %{})
   end
 
+  @spec get_shared_secret_auth(pos_integer(), pos_integer()) ::
+          {:ok, SharedSecretAuth.t()} | {:error, :not_found}
   def get_shared_secret_auth(product_id, auth_id) do
     SharedSecretAuth
     |> join(:inner, [ssa], p in assoc(ssa, :product))
@@ -162,6 +174,7 @@ defmodule NervesHub.Products do
     end
   end
 
+  @spec get_shared_secret_auth(String.t()) :: {:ok, SharedSecretAuth.t()} | {:error, :not_found}
   def get_shared_secret_auth(key) do
     SharedSecretAuth
     |> join(:inner, [ssa], p in assoc(ssa, :product))
@@ -175,18 +188,23 @@ defmodule NervesHub.Products do
     end
   end
 
+  @spec load_shared_secret_auth(Product.t()) :: Product.t()
   def load_shared_secret_auth(product) do
     product
     |> Ecto.reset_fields([:shared_secret_auths])
     |> Repo.preload(:shared_secret_auths)
   end
 
+  @spec create_shared_secret_auth(Product.t()) ::
+          {:ok, SharedSecretAuth.t()} | {:error, Ecto.Changeset.t()}
   def create_shared_secret_auth(product) do
     product
     |> SharedSecretAuth.create_changeset()
     |> Repo.insert()
   end
 
+  @spec deactivate_shared_secret_auth(Product.t(), pos_integer()) ::
+          {:ok, SharedSecretAuth.t()} | {:error, Ecto.Changeset.t()}
   def deactivate_shared_secret_auth(product, shared_secret_id) do
     {:ok, auth} = get_shared_secret_auth(product.id, shared_secret_id)
 
@@ -195,6 +213,7 @@ defmodule NervesHub.Products do
     |> Repo.update()
   end
 
+  @spec devices_csv(Product.t()) :: binary()
   def devices_csv(%Product{} = product) do
     product = Repo.preload(product, [:org, devices: :device_certificates])
     data = Enum.map(product.devices, &device_csv_line(&1, product))
