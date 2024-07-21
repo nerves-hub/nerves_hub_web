0a335ca54adf89f33d8a0256033b7730284b92e1
Josh Kalderimis <josh.kalderimis@gmail.com>
2024-07-21 12:51:32 +1200

    Switch to a single Regex run

--- lib/nerves_hub/fwup.ex
+++ lib/nerves_hub/fwup.ex
@@ -3,6 +3,11 @@ defmodule NervesHub.Fwup do
   Helpers for dealing with files created by FWUP.
   """
 
+  @required_metadata_keys [:architecture, :platform, :product, :uuid, :version]
+  @optional_metadata_keys [:author, :description, :misc, :vcs_identifier]
+
+  @metadata_regex Regex.compile!("meta-(?<key>[^\n]+)=\"(?<value>[^\n]+)\"")
+
   @doc """
   Decode and parse metadata from a FWUP file.
   """
@@ -12,28 +17,11 @@ defmodule NervesHub.Fwup do
           | {:error, :invalid_metadata}
   def metadata(file_path) do
     with {:ok, metadata} <- get_metadata(file_path),
-         {:ok, uuid} <- metadata_value(metadata, "meta-uuid"),
-         {:ok, architecture} <- metadata_value(metadata, "meta-architecture"),
-         {:ok, platform} <- metadata_value(metadata, "meta-platform"),
-         {:ok, product} <- metadata_value(metadata, "meta-product"),
-         {:ok, version} <- metadata_value(metadata, "meta-version"),
-         {:ok, author} <- metadata_value(metadata, "meta-author", nil),
-         {:ok, description} <- metadata_value(metadata, "meta-description", nil),
-         {:ok, misc} <- metadata_value(metadata, "meta-misc", nil),
-         {:ok, vcs_identifier} <- metadata_value(metadata, "meta-vcs-identifier", nil) do
-      metadata = %{
-        architecture: architecture,
-        author: author,
-        description: description,
-        misc: misc,
-        platform: platform,
-        product: product,
-        uuid: uuid,
-        vcs_identifier: vcs_identifier,
-        version: version
-      }
-
-      {:ok, metadata}
+         parsed_metadata <- parse_metadata(metadata),
+         {:ok, required_metadata} <- required_values(parsed_metadata),
+         optional_metadata <- optional_values(parsed_metadata),
+         complete <- Map.merge(required_metadata, optional_metadata) do
+      {:ok, complete}
     end
   end
 
@@ -47,25 +35,36 @@ defmodule NervesHub.Fwup do
     end
   end
 
-  defp metadata_value(metadata, key) when is_binary(key) do
-    {:ok, regex} = "#{key}=\"(?<value>[^\n]+)\"" |> Regex.compile()
+  defp parse_metadata(metadata) do
+    Regex.scan(@metadata_regex, metadata)
+    |> Enum.reduce(%{}, fn line, acc ->
+      [_, key, value] = line
 
-    case Regex.named_captures(regex, metadata) do
-      %{"value" => value} ->
-        {:ok, value}
+      key =
+        key
+        |> String.replace("-", "_")
+        |> String.to_atom()
 
-      _ ->
-        {:error, :invalid_metadata}
+      Map.put(acc, key, value)
+    end)
+  end
+
+  defp required_values(metadata) do
+    slice = Map.take(metadata, @required_metadata_keys)
+    slice_keys = Map.keys(slice)
+
+    if Enum.sort(slice_keys) == @required_metadata_keys do
+      {:ok, slice}
+    else
+      {:error, :invalid_metadata}
     end
   end
 
-  defp metadata_value(metadata, key, default) when is_binary(key) do
-    case metadata_value(metadata, key) do
-      {:ok, metadata_item} ->
-        {:ok, metadata_item}
+  defp optional_values(metadata) do
+    defaults = Map.new(@optional_metadata_keys, fn x -> {x, nil} end)
 
-      {:error, :invalid_metadata} ->
-        {:ok, default}
-    end
+    slice = Map.take(metadata, @optional_metadata_keys)
+
+    Map.merge(defaults, slice)
   end
 end
