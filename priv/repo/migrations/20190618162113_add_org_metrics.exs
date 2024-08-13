09288344c63d9f7b4d048318c00228b71188e202
Josh Kalderimis <josh.kalderimis@gmail.com>
2024-08-13 16:36:59 +1200

    Fix an incorrect usage of `references` in the migrations
    This was causing a `(KeyError) key :null not found in: %Ecto.Migration.Reference` error which is now being enforced by `ecto_sql`

--- priv/repo/migrations/20190618162113_add_org_metrics.exs
+++ priv/repo/migrations/20190618162113_add_org_metrics.exs
@@ -4,7 +4,7 @@ defmodule NervesHub.Repo.Migrations.AddOrgMetrics do
   def change do
     create table(:org_metrics, primary_key: false) do
       add(:id, :uuid, primary_key: true)
-      add(:org_id, references(:orgs, null: false))
+      add(:org_id, references(:orgs), null: false)
       add(:devices, :integer, null: false)
       add(:bytes_transferred, :integer, null: false)
       add(:bytes_stored, :integer, null: false)
