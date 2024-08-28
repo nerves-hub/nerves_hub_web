4b51d616f7556bb1ec499e0fef47713391ae5790
Eric Oestrich <eric@oestrich.org>
2024-08-28 11:42:08 -0400

    Disconnect updating a deployment and recalculating devices
    We no longer want each device to be processing this at the same time as
    other devices. This causes massive database contention and can crash a
    node.
    
    # Conflicts:
    #	lib/nerves_hub_web/channels/device_channel.ex

--- lib/nerves_hub/deployments.ex
+++ lib/nerves_hub/deployments.ex
@@ -134,53 +134,9 @@ defmodule NervesHub.Deployments do
       {:ok, deployment} ->
         deployment = Repo.preload(deployment, [:firmware], force: true)
 
-        payload = %{
-          id: deployment.id,
-          active: deployment.is_active,
-          product_id: deployment.product_id,
-          platform: deployment.firmware.platform,
-          architecture: deployment.firmware.architecture,
-          version: deployment.firmware.version,
-          conditions: deployment.conditions
-        }
-
         # if the conditions changed, we should reset all devices and tell any connected
         if Map.has_key?(changeset.changes, :conditions) do
-          Device
-          |> where([d], d.deployment_id == ^deployment.id)
-          |> Repo.update_all(set: [deployment_id: nil])
-
-          if deployment.conditions["version"] in [nil, ""] and deployment.is_active do
-            # The version condition is the only one not done with the DB.
-            # This opens up a minor optimization to preemptively set matching
-            # devices to the new deployment all at once since the version
-            # condition can be skipped.
-            #
-            # This also helps with offline devices by potentially reducing the
-            # need to do the expensive deployment check on next connect which
-            # reduces the load when a lot of devices come online at once.
-            Device
-            |> where([d], d.product_id == ^deployment.product_id)
-            |> where(
-              [d],
-              fragment("?->>'platform' = ?", d.firmware_metadata, ^deployment.firmware.platform)
-            )
-            |> where(
-              [d],
-              fragment(
-                "?->>'architecture' = ?",
-                d.firmware_metadata,
-                ^deployment.firmware.architecture
-              )
-            )
-            |> where([d], fragment("? <@ ?", ^deployment.conditions["tags"], d.tags))
-            |> Repo.update_all(set: [deployment_id: deployment.id])
-          end
-
-          _ = broadcast(deployment, "deployments/changed", payload)
-          _ = broadcast(:none, "deployments/changed", payload)
-
-          description = "deployment #{deployment.name} conditions changed and removed all devices"
+          description = "deployment #{deployment.name} conditions changed"
           AuditLogs.audit!(deployment, deployment, description)
         end
 
@@ -199,15 +155,7 @@ defmodule NervesHub.Deployments do
         # if is_active is false, wipe it out like above
         # if its now true, tell the none deployment devices
         if Map.has_key?(changeset.changes, :is_active) do
-          if deployment.is_active do
-            broadcast(:none, "deployments/changed", payload)
-          else
-            Device
-            |> where([d], d.deployment_id == ^deployment.id)
-            |> Repo.update_all(set: [deployment_id: nil])
-
-            _ = broadcast(deployment, "deployments/changed", payload)
-
+          if !deployment.is_active do
             description = "deployment #{deployment.name} is inactive and removed all devices"
             AuditLogs.audit!(deployment, deployment, description)
           end
