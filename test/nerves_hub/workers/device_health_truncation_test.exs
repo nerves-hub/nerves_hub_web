38116348372421c50746e0f3019d012677d49180
Elin Olsson <elin@underjord.io>
2024-09-09 15:30:15 +0200

    Keep tests up to date with new metrics approach
    - Update worker and liveview test
    - Correct accidental changes to KeyControllerTest

--- test/nerves_hub/workers/device_health_truncation_test.exs
+++ test/nerves_hub/workers/device_health_truncation_test.exs
@@ -5,7 +5,7 @@ defmodule NervesHub.Workers.DeviceHealthTruncationTest do
   alias NervesHub.Devices
   alias NervesHub.Workers.DeviceHealthTruncation
 
-  test "delete device health entries older than 7 days", %{tmp_dir: dir} do
+  test "delete device health and metrics entries older than 7 days", %{tmp_dir: dir} do
     user = Fixtures.user_fixture()
     org = Fixtures.org_fixture(user)
     product = Fixtures.product_fixture(user, org)
@@ -16,18 +16,29 @@ defmodule NervesHub.Workers.DeviceHealthTruncationTest do
     for x <- 0..9 do
       days_ago = DateTime.shift(DateTime.utc_now(), day: -x)
 
-      inserted =
+      inserted_health =
         %{"device_id" => device.id, "data" => %{"literally_any_map" => "values"}}
         |> Devices.DeviceHealth.save()
         |> Ecto.Changeset.put_change(:inserted_at, days_ago)
         |> Repo.insert()
 
-      assert {:ok, %Devices.DeviceHealth{}} = inserted
+      assert {:ok, %Devices.DeviceHealth{}} = inserted_health
+
+      inserted_metric =
+        %{"device_id" => device.id, "key" => "cpu_temp", "value" => 41.381}
+        |> Devices.DeviceMetric.save()
+        |> Ecto.Changeset.put_change(:inserted_at, days_ago)
+        |> Repo.insert()
+
+      assert {:ok, %Devices.DeviceMetric{}} = inserted_metric
     end
 
     assert :ok = perform_job(DeviceHealthTruncation, %{})
 
     healths = Devices.get_device_health(device.id)
     assert 7 = Enum.count(healths)
+
+    metrics = Devices.Metrics.get_device_metrics(device.id)
+    assert 7 = Enum.count(metrics)
   end
 end
--- test/nerves_hub_web/live/devices/health_test.exs
+++ test/nerves_hub_web/live/devices/health_test.exs
@@ -1,8 +1,7 @@
 defmodule NervesHubWeb.Devices.HealthTest do
   use NervesHubWeb.ConnCase.Browser, async: false
 
-  alias NervesHub.Devices
-  alias NervesHubWeb.Live.Devices.DeviceHealth
+  alias NervesHub.Devices.Metrics
 
   alias NervesHubWeb.Endpoint
 
@@ -28,83 +27,20 @@ defmodule NervesHubWeb.Devices.HealthTest do
     product: product,
     device: device
   } do
-    device_health = %{
-      "device_id" => device.id,
-      "data" => %{
-        "metrics" => %{
-          "cpu_temp" => 41.381,
-          "load_15min" => 0.06,
-          "load_1min" => 0.55,
-          "load_5min" => 0.15,
-          "size_mb" => 7892,
-          "used_mb" => 172,
-          "used_percent" => 2
-        },
-        "timestamp" => "2024-08-26T15:44:18.295149Z"
-      }
+    metrics = %{
+      "cpu_temp" => 41.381,
+      "load_15min" => 0.06,
+      "load_1min" => 0.55,
+      "load_5min" => 0.15,
+      "size_mb" => 7892,
+      "used_mb" => 172,
+      "used_percent" => 2
     }
 
-    assert {:ok, %Devices.DeviceHealth{}} = Devices.save_device_health(device_health)
+    assert {:ok, _} = Metrics.save_metrics(device.id, metrics)
 
     conn
     |> visit("/org/#{org.name}/#{product.name}/devices/#{device.identifier}/health")
     |> assert_has("svg")
-  end
-
-  test "assert health data without metrics doesn't crash liveview", %{
-    conn: conn,
-    org: org,
-    product: product,
-    device: device
-  } do
-    device_health = %{
-      "device_id" => device.id,
-      "data" => %{
-        "timestamp" => "2024-08-26T15:44:18.295149Z"
-      }
-    }
-
-    assert {:ok, %Devices.DeviceHealth{}} = Devices.save_device_health(device_health)
-
-    conn
-    |> visit("/org/#{org.name}/#{product.name}/devices/#{device.identifier}/health")
-    |> assert_has(".metrics-text", text: "No data for selected period")
-  end
-
-  test "assert metrics with nil data is rejected", %{
-    device: device
-  } do
-    valid_data = %{
-      "device_id" => device.id,
-      "data" => %{
-        "metrics" => %{
-          "cpu_temp" => 41.381,
-          "load_15min" => 0.06,
-          "load_1min" => 0.55,
-          "load_5min" => 0.15,
-          "size_mb" => 7892,
-          "used_mb" => 172,
-          "used_percent" => 2
-        },
-        "timestamp" => "2024-08-26T15:44:18.295149Z"
-      }
-    }
-
-    invalid_data = %{
-      "device_id" => device.id,
-      "data" => %{
-        "timestamp" => "2024-08-26T15:44:18.295149Z"
-      }
-    }
-
-    assert {:ok, %Devices.DeviceHealth{}} = Devices.save_device_health(valid_data)
-    assert {:ok, %Devices.DeviceHealth{}} = Devices.save_device_health(invalid_data)
-
-    metrics =
-      device.id
-      |> Devices.get_device_health()
-      |> DeviceHealth.organize_data()
-
-    assert length(metrics.cpu_temp) == 1
   end
 end
