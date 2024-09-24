80f7e3d62479045d97d211291161a424d832dca6
Josh Kalderimis <josh.kalderimis@gmail.com>
2024-09-25 06:08:29 +1200

    Allow the deployment calculator interval to be changed (#1537)
    * Allow the deployment calculator interval to be changed
    
    * Set the default interval to 1 hour
    
    Co-authored-by: Frank Hunleth <fhunleth@troodon-software.com>
    
    ---------
    
    Co-authored-by: Frank Hunleth <fhunleth@troodon-software.com>

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,6 +33,8 @@ config :nerves_hub,
     String.to_integer(System.get_env("DEVICE_DEPLOYMENT_CHANGE_JITTER_SECONDS", "10")),
   device_last_seen_update_interval_minutes:
     String.to_integer(System.get_env("DEVICE_LAST_SEEN_UPDATE_INTERVAL_MINUTES", "5")),
+  deployment_calculator_interval_seconds:
+    String.to_integer(System.get_env("DEPLOYMENT_CALCULATOR_INTERVAL_SECONDS", "3600")),
   mapbox_access_token: System.get_env("MAPBOX_ACCESS_TOKEN"),
   dashboard_enabled: System.get_env("DASHBOARD_ENABLED", "false") == "true"
 
