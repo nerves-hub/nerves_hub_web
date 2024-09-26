6409aa73949549027583dade65ae24a1af8a0248
Josh Kalderimis <josh.kalderimis@gmail.com>
2024-09-27 11:23:06 +1200

    Don't log `mfa` and `pid`, it's just noise

--- lib/nerves_hub/logger_formatter.ex
+++ lib/nerves_hub/logger_formatter.ex
@@ -1,7 +1,7 @@
 defmodule NervesHub.LoggerFormatter do
   @pattern Logger.Formatter.compile("$time [$level] $metadata$message\n")
   def format(level, message, timestamp, metadata) do
-    metadata = Keyword.drop(metadata, [:line, :file, :domain, :application])
+    metadata = Keyword.drop(metadata, [:line, :file, :domain, :application, :mfa, :pid])
     Logger.Formatter.format(@pattern, level, message, timestamp, metadata)
   end
 end
