871b91369c5dd16de4e82904bdc1d275a13ec363
Josh Kalderimis <josh.kalderimis@gmail.com>
2024-10-01 15:37:09 +1300

    Remove node count reporting

--- lib/nerves_hub/metrics_poller.ex
+++ lib/nerves_hub/metrics_poller.ex
@@ -2,19 +2,13 @@ defmodule NervesHub.MetricsPoller do
   def child_spec() do
     {:telemetry_poller,
      measurements: [
-       {NervesHub.MetricsPoller, :dispatch_node_count, []},
-       {NervesHub.MetricsPoller, :dispatch_device_count, []}
+       {NervesHub.MetricsPoller, :report_device_count, []}
      ],
      period: :timer.seconds(60),
      name: :nerves_hub_poller}
   end
 
-  def dispatch_node_count() do
-    nodes = Node.list()
-    :telemetry.execute([:nerves_hub, :nodes], %{count: Enum.count(nodes)}, %{nodes: nodes})
-  end
-
-  def dispatch_device_count() do
+  def report_device_count() do
     device_count = Registry.count(NervesHub.Devices.Registry)
     :telemetry.execute([:nerves_hub, :devices, :online], %{count: device_count}, %{node: node()})
   end
