2ba0f1dcb6597bc5086010e19e18f9951429b13d
Lars Wikman <lars@underjord.io>
2024-10-12 07:52:59 +0200

    Add option for showing updated devices on the map (#1592)
    * Add option for showing updated devices on the map
    
    This adds a way to indicate if the firmware is fully up to date on
    devices for the map.
    
    It also cleans up a few performance aspects of the map:
    - Don't block initial load on fetching the devices/markers
      as it can be slow for many devices
    - Change query to be more specific and not fetch as many unused devices
    - Change data-plumbing so that markers are sent over push_event instead
      of via attributes with extra JSON encode/decode.
    
    The map should load significantly faster and will indicate "Loading..."
    while loading if delayed.
    
    We should further check index usage for these new queries as I think
    they could be made even faster if they don't have indexes today.
    
    * Remove offline devices from firmware update map
    
    * Remove repeating subscribes
    
    * Fix refresh to avoid unnecessary memory usage
    
    Change re-subscribing to be done properly but never in duplicates.
    Delay multiple refreshes to never happen more frequently than 500ms.
    Default refresh at 5000ms.

--- lib/nerves_hub_web/live/dashboard/index.ex
+++ lib/nerves_hub_web/live/dashboard/index.ex
@@ -3,8 +3,12 @@ defmodule NervesHubWeb.Live.Dashboard.Index do
 
   alias NervesHub.Devices
   alias NervesHub.Devices.Device
+  alias NervesHub.Deployments
 
   alias Phoenix.Socket.Broadcast
+
+  @default_refresh 5000
+  @delay 500
 
   @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
@@ -12,83 +16,116 @@ defmodule NervesHubWeb.Live.Dashboard.Index do
 
     socket
     |> page_title("Dashboard - #{product.name}")
-    |> assign_devices_and_markers()
-    |> subscribe_to_devices()
-    |> start_refresh_cycle()
+    |> assign(:time, time())
+    |> assign(:next_timer, nil)
+    |> assign(:loading?, true)
+    |> assign(:mode, "updated")
+    |> assign(:device_count, 0)
+    |> assign(:marker_count, 0)
+    |> refresh_after(1)
     |> ok()
   end
 
   @impl Phoenix.LiveView
   def handle_info(%Broadcast{event: "location:updated"}, socket) do
     socket
-    |> assign_devices_and_markers()
+    |> update_devices_and_markers()
     |> noreply()
   end
 
   def handle_info(%Broadcast{event: "connection:status"}, socket) do
     socket
-    |> assign_devices_and_markers()
+    |> update_devices_and_markers()
     |> noreply()
   end
 
   def handle_info(%Broadcast{event: "connection:change"}, socket) do
     socket
-    |> assign_devices_and_markers()
+    |> update_devices_and_markers()
     |> noreply()
   end
 
   def handle_info(:refresh_device_list, socket) do
-    Process.send_after(self(), :refresh_device_list, 5000)
-
     socket
-    |> assign_devices_and_markers()
-    |> subscribe_to_devices()
+    |> update_devices_and_markers()
     |> noreply()
   end
 
   # Ignore unused messages from the subscription
   def handle_info(_unknown, socket), do: {:noreply, socket}
 
-  defp start_refresh_cycle(socket) do
-    Process.send_after(self(), :refresh_device_list, 5000)
-    socket
+  @impl Phoenix.LiveView
+  def handle_event("map_ready", _params, socket) do
+    socket = update_devices_and_markers(socket)
+    {:noreply, socket}
   end
 
-  defp subscribe_to_devices(socket) do
-    if connected?(socket) do
-      Enum.each(socket.assigns.devices, fn device ->
-        socket.endpoint.subscribe("device:#{device.identifier}:internal")
-      end)
+  def handle_event("filter-change", %{"mode" => mode}, socket) do
+    {:noreply, assign(socket, mode: mode)}
+  end
 
-      socket
-    else
-      socket
+  defp subscribe_to_devices(socket, devices) do
+    if connected?(socket) do
+      Enum.each(devices, fn device ->
+        if not subscribed?(device) do
+          socket.endpoint.subscribe("device:#{device.identifier}:internal")
+        end
+      end)
     end
   end
 
-  defp assign_devices_and_markers(%{assigns: %{org: org, product: product}} = socket) do
-    devices = Devices.get_devices_by_org_id_and_product_id(org.id, product.id)
+  defp subscribed?(device) do
+    Registry.count_select(NervesHub.PubSub, [
+      {{"device:#{device.identifier}:internal", self(), :_}, [], [true]}
+    ]) > 0
+  end
 
-    map_markers =
-      Enum.reduce(devices, [], fn device, acc ->
-        generate_map_marker(device, acc)
-      end)
+  defp update_devices_and_markers(%{assigns: %{org: org, product: product}} = socket) do
+    t = time()
+    duration = t - socket.assigns.time
 
-    socket
-    |> assign(:devices, devices)
-    |> assign(:map_markers, map_markers)
+    if duration >= @delay do
+      devices = Devices.get_minimal_device_location_by_org_id_and_product_id(org.id, product.id)
+
+      latest_firmwares =
+        Deployments.get_deployments_by_product(product.id)
+        |> Enum.reduce(%{}, fn deployment, acc ->
+          Map.put(acc, deployment.firmware.uuid, deployment.firmware.platform)
+        end)
+
+      map_markers =
+        Enum.reduce(devices, [], fn device, acc ->
+          generate_map_marker(device, acc, latest_firmwares)
+        end)
+
+      subscribe_to_devices(socket, devices)
+
+      socket
+      |> assign(:time, t)
+      |> assign(:loading?, false)
+      |> assign(:device_count, Enum.count(devices))
+      |> assign(:marker_count, Enum.count(map_markers))
+      |> push_event("markers", %{markers: map_markers})
+      # send a refresh on the normal interval after an update
+      |> refresh_after(@default_refresh)
+    else
+      socket
+      # send a refresh after the delay, meaning as soon as possible after
+      |> refresh_after(@delay - duration)
+    end
   end
 
   defp generate_map_marker(
-         %Device{
+         %{
            id: id,
            identifier: identifier,
            connection_status: connection_status,
-           connection_metadata: %{
-             "location" => %{"longitude" => longitude, "latitude" => latitude}
-           }
+           longitude: longitude,
+           latitude: latitude,
+           firmware_uuid: firmware_uuid
          },
-         markers
+         markers,
+         latest_firmwares
        )
        when is_number(longitude) and is_number(latitude) do
     new_marker =
@@ -96,16 +133,30 @@ defmodule NervesHubWeb.Live.Dashboard.Index do
         id: id,
         identifier: identifier,
         status: get_connection_status(connection_status),
+        latest_firmware: Map.has_key?(latest_firmwares, firmware_uuid),
         location: %{"longitude" => longitude, "latitude" => latitude}
       }
 
     [new_marker | markers]
   end
 
-  defp generate_map_marker(%Device{} = _device, markers) do
+  defp generate_map_marker(%Device{} = _device, markers, _) do
     markers
   end
 
   defp get_connection_status(:connected), do: "connected"
   defp get_connection_status(_), do: "offline"
+
+  defp refresh_after(socket, delay) do
+    if socket.assigns.next_timer do
+      Process.cancel_timer(socket.assigns.next_timer)
+    end
+
+    timer = Process.send_after(self(), :refresh_device_list, delay)
+    assign(socket, :next_timer, timer)
+  end
+
+  defp time do
+    System.monotonic_time(:millisecond)
+  end
 end
