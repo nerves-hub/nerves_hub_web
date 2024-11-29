6c5291e8ce5b46b47423cf99205c632225e85493
Lars Wikman <lars@underjord.io>
2024-11-29 11:49:48 +0100

    Implement new UI for bulk actions on device list

--- lib/nerves_hub_web/live/devices/index.ex
+++ lib/nerves_hub_web/live/devices/index.ex
@@ -196,6 +196,7 @@ defmodule NervesHubWeb.Live.Devices.Index do
   end
 
   def handle_event("select", %{"id" => id_str}, socket) do
+    IO.inspect(id_str)
     id = String.to_integer(id_str)
     selected_devices = socket.assigns.selected_devices
 
