991139a22312307adbc87a588a97b2681974a03d
Elin Olsson <elin@underjord.io>
2024-12-05 07:49:44 +0100

    Fix broken liveview

--- lib/nerves_hub_web/live/devices/index.ex
+++ lib/nerves_hub_web/live/devices/index.ex
@@ -92,7 +92,7 @@ defmodule NervesHubWeb.Live.Devices.Index do
     |> assign(:device_tags, "")
     |> assign(:total_entries, 0)
     |> assign(:current_alarms, Alarms.get_current_alarm_types(product.id))
-    |> assign(:metrics_keys, Metrics.default_metric_types())
+    |> assign(:metrics_keys, Metrics.default_metrics())
     |> subscribe_and_refresh_device_list_timer()
     |> ok(:sidebar)
   end
