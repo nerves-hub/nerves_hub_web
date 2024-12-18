09ee7f51d592d1248930b7ff18900cf5ee040c98
Josh Kalderimis <josh.kalderimis@gmail.com>
2024-12-19 10:34:00 +1300

    Check the DB during `/status/alive` calls (#1705)

--- lib/nerves_hub_web/plugs/im_alive.ex
+++ lib/nerves_hub_web/plugs/im_alive.ex
@@ -9,11 +9,18 @@ defmodule NervesHubWeb.Plugs.ImAlive do
   def init(config), do: config
 
   def call(%{request_path: "/status/alive"} = conn, _) do
-    conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(200, "Hello Friend!")
-    |> halt
+    case Ecto.Adapters.SQL.query(NervesHub.Repo, "SELECT true", []) do
+      {:ok, _} -> send_result(conn, 200, "Hello, Friend!")
+      _result -> send_result(conn, 500, "Sorry, Friend :(")
+    end
   end
 
   def call(conn, _), do: conn
+
+  defp send_result(conn, code, message) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(code, message)
+    |> halt
+  end
 end
