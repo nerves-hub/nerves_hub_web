573ab967a1ddad53f408f85d3d23d3733ba1c303
Josh Kalderimis <josh.kalderimis@gmail.com>
2025-01-08 12:42:02 +1300

    Use consistent exception naming

--- lib/nerves_hub_web/errors.ex
+++ lib/nerves_hub_web/errors.ex
@@ -2,6 +2,6 @@ defmodule NervesHubWeb.NotFoundError do
   defexception message: "not found", plug_status: 404
 end
 
-defmodule NervesHubWeb.Unauthorized do
+defmodule NervesHubWeb.UnauthorizedError do
   defexception message: "forbidden", plug_status: 401
 end
--- test/nerves_hub_web/plugs/api/user_test.exs
+++ test/nerves_hub_web/plugs/api/user_test.exs
@@ -27,14 +27,14 @@ defmodule NervesHubWeb.API.Plugs.UserTest do
   end
 
   test "rejects unknown API token" do
-    assert_raise(NervesHubWeb.Unauthorized, fn ->
+    assert_raise(NervesHubWeb.UnauthorizedError, fn ->
       build_conn()
       |> put_req_header("authorization", "token wat-is-this-token")
       |> put_req_header("accept", "application/json")
       |> get("/api/users/me")
     end)
 
-    assert_raise(NervesHubWeb.Unauthorized, fn ->
+    assert_raise(NervesHubWeb.UnauthorizedError, fn ->
       build_conn()
       |> put_req_header("authorization", "token nhu_1234567890abcdefghijklmnopqrstuvwxyz")
       |> put_req_header("accept", "application/json")
