533fde8cc1f6ddbdffa96c04508d679eb9ce8e8e
Josh Kalderimis <josh.kalderimis@gmail.com>
2025-01-10 16:59:04 +1300

    Restrict large uploads to the firmwares api route

--- lib/nerves_hub_web/dymanic_config_multipart.ex
+++ lib/nerves_hub_web/dymanic_config_multipart.ex
@@ -3,6 +3,8 @@ defmodule NervesHubWeb.DymanicConfigMultipart do
   A wrapper around `Plug.Parsers.MULTIPART` which allows for the `:length` opt (max file size)
   to be set during runtime.
 
+  This also restricts large file uploads to the firmware upload api route.
+
   This can later be expanded to allow for different file size limits based on the organization.
 
   Thank you to https://hexdocs.pm/plug/Plug.Parsers.MULTIPART.html#module-dynamic-configuration
@@ -16,7 +18,7 @@ defmodule NervesHubWeb.DymanicConfigMultipart do
   end
 
   def parse(conn, "multipart", subtype, headers, opts) do
-    opts = @multipart.init([length: max_file_size()] ++ opts)
+    opts = @multipart.init([length: max_file_size(conn)] ++ opts)
     @multipart.parse(conn, "multipart", subtype, headers, opts)
   end
 
@@ -24,7 +26,11 @@ defmodule NervesHubWeb.DymanicConfigMultipart do
     {:next, conn}
   end
 
-  defp max_file_size() do
-    Application.get_env(:nerves_hub, NervesHub.Firmwares.Upload, [])[:max_size]
+  defp max_file_size(conn) do
+    if String.match?(conn.request_path, ~r/^\/api\/orgs\/\w+\/products\/\w+\/firmwares$/) do
+      Application.get_env(:nerves_hub, NervesHub.Firmwares.Upload, [])[:max_size]
+    else
+      1_000_000
+    end
   end
 end
