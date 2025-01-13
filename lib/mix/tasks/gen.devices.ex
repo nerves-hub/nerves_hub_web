c22c54694ded98635d6bffcaeec196be55fb61a0
Josh Kalderimis <josh.kalderimis@gmail.com>
2025-01-14 08:57:31 +1300

    Mix task best practises (#1756)
    Adding a `@moduledoc` to the tasks means they are now listed when using
    `mix help`, plus it has the added advantage of showing more information
    when using `mix help nerves_hub.gen.devices` or `mix help
    nerves_hub.gen.metrics`
    
    Oh, and I have made the task naming consistent, and given them a
    `@preferred_cli_env` of `:dev`
    
    <img width="731" alt="Screenshot 2025-01-12 at 10 10 15 PM"
    src="https://github.com/user-attachments/assets/fd270ecd-ffb8-440b-af54-49886082d766"
    />

--- lib/mix/tasks/gen.devices.ex
+++ lib/mix/tasks/gen.devices.ex
@@ -1,18 +1,31 @@
-defmodule Mix.Tasks.Gen.Devices do
-  @moduledoc false
+defmodule Mix.Tasks.NervesHub.Gen.Devices do
+  @moduledoc """
+  Generate a collection of devices for an Organization and Product.
+
+  ## Examples
+
+      mix nerves_hub.gen.devices my-org my-product 1000
+  """
+
+  @shortdoc "Generate a collection of devices"
 
   use Mix.Task
 
-  @shortdoc "Generate a mass of devices"
+  alias NervesHub.Accounts
+  alias NervesHub.Devices
+  alias NervesHub.Products
 
+  @requirements ["app.start"]
+  @preferred_cli_env :dev
+
+  @impl Mix.Task
   def run([org_name, product_name, count]) do
-    Mix.Task.run("app.start")
     count = String.to_integer(count)
-    {:ok, org} = NervesHub.Accounts.get_org_by_name(org_name)
-    {:ok, product} = NervesHub.Products.get_product_by_org_id_and_name(org.id, product_name)
+    {:ok, org} = Accounts.get_org_by_name(org_name)
+    {:ok, product} = Products.get_product_by_org_id_and_name(org.id, product_name)
 
     current_count =
-      NervesHub.Devices.get_device_count_by_org_id_and_product_id(org.id, product.id)
+      Devices.get_device_count_by_org_id_and_product_id(org.id, product.id)
 
     current_count..(current_count + count)
     |> Enum.map(fn i ->
@@ -23,7 +36,7 @@ defmodule Mix.Tasks.Gen.Devices do
       lng = -180..180 |> Enum.random()
       lat = -90..90 |> Enum.random()
 
-      NervesHub.Devices.create_device(%{
+      Devices.create_device(%{
         org_id: org.id,
         product_id: product.id,
         identifier: "generated-#{i}",
