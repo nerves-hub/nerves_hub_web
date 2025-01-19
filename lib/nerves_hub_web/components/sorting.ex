dd539e9f2edfca60f46eb62eb93424627649e7b7
Josh Kalderimis <josh.kalderimis@gmail.com>
2025-01-20 00:06:24 +1300

    Tweak the sorting icon logic

--- lib/nerves_hub_web/components/sorting.ex
+++ lib/nerves_hub_web/components/sorting.ex
@@ -13,7 +13,7 @@ defmodule NervesHubWeb.Components.Sorting do
       <svg :if={@selected_field != @field} class="ml-auto invisible group-hover:visible w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M12.5 7.5L10 5L7.5 7.5M12.5 12.5L10 15L7.5 12.5" stroke="#71717A" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
       </svg>
-      <svg :if={@selected_field == @field && @selected_direction == "asc"} class="ml-auto w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg :if={@selected_field == @field && (@selected_direction == "asc" || is_nil(@selected_direction))} class="ml-auto w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M17 14L12 9L7 14" stroke="#71717A" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
       </svg>
       <svg :if={@selected_field == @field && @selected_direction == "desc"} class="ml-auto w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
