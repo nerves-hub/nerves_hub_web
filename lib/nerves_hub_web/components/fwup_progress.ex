8ab6c5a260a2a263bf9b458af4243cb150e8da9e
Lars Wikman <lars@underjord.io>
2025-01-19 12:08:55 +0100

    Add very shiny progress bar for new UI

--- lib/nerves_hub_web/components/fwup_progress.ex
+++ lib/nerves_hub_web/components/fwup_progress.ex
@@ -5,27 +5,11 @@ defmodule NervesHubWeb.Components.FwupProgress do
 
   def render(assigns) do
     ~H"""
-    <div class="help-text mt-3">Progress</div>
-    <div class="progress device-show">
-      <div class="progress-bar" role="progressbar" style={"width: #{@fwup_progress}%"}>
-        {@fwup_progress}%
+    <div class="sticky top-0 w-full h-0 overflow-visible z-20">
+      <div class="absolute border-0 border-t-[1px] border-success-500" role="progressbar" style={"width: #{@fwup_progress}%"}>
+        <div class="animate-pulse bg-progress-glow w-full h-16" />
       </div>
-    </div>
-    """
-  end
-
-  attr(:fwup_progress, :any)
-
-  def updated_render(assigns) do
-    ~H"""
-    <div class="flex flex-col w-1/2 pl-4 pr-2">
-      <div class="flex justify-between mb-1">
-        <span class="text-sm font-base text-indigo-500 dark:text-white">Update in progress</span>
-        <span class="text-base font-medium text-indigo-500 dark:text-white">{@fwup_progress}%</span>
-      </div>
-      <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-        <div class="bg-indigo-500 h-2.5 rounded-full animate-pulse" style={"width: #{@fwup_progress}%"}></div>
-      </div>
+      <div class="text-center pt-2 text-sm animate-pulse">Updating firmware <%= @fwup_progress %>%</div>
     </div>
     """
   end
