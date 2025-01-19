90e5fb5152b8fd2ea5250708968e8c06dfab810f
Josh Kalderimis <josh.kalderimis@gmail.com>
2025-01-20 01:16:41 +1300

    Some opacity tweaks
    Plus bring back the old progress function which is used by the old UI

--- lib/nerves_hub_web/components/fwup_progress.ex
+++ lib/nerves_hub_web/components/fwup_progress.ex
@@ -5,11 +5,26 @@ defmodule NervesHubWeb.Components.FwupProgress do
 
   def render(assigns) do
     ~H"""
-    <div class="sticky top-0 w-full h-0 overflow-visible z-20">
-      <div class="absolute border-0 border-t-[1px] border-success-500" role="progressbar" style={"width: #{@fwup_progress}%"}>
+    <div class="help-text mt-3">Progress</div>
+    <div class="progress device-show">
+      <div class="progress-bar" role="progressbar" style={"width: #{@fwup_progress}%"}>
+        {@fwup_progress}%
+      </div>
+    </div>
+    """
+  end
+
+  attr(:fwup_progress, :any)
+
+  def updated_render(assigns) do
+    ~H"""
+    <div class="relative sticky top-0 w-full h-0 overflow-visible z-20">
+      <div class="z-40 absolute border-0 border-t-[1px] border-success-500" role="progressbar" style={"width: #{@fwup_progress}%"}>
         <div class="animate-pulse bg-progress-glow w-full h-16" />
       </div>
-      <div class="text-center pt-2 text-sm animate-pulse">Updating firmware {@fwup_progress}%</div>
+      <div class="z-50 absolute w-full flex justify-center">
+        <div class="mt-1 py-1 px-2 bg-base-900/20 rounded-full text-sm font-medium">Updating firmware {@fwup_progress}%</div>
+      </div>
     </div>
     """
   end
