a270f4b7f13a5b76ef03349bfbb7e0abf1176d46
Josh Kalderimis <josh.kalderimis@gmail.com>
2025-02-13 11:11:30 +1300

    Remove an index migration that didn't do what we needed

--- priv/repo/migrations/20250211234552_create_device_distributed_orchestrator_index.exs
+++
@@ -1,7 +0,0 @@
-defmodule Elixir.NervesHub.Repo.Migrations.CreateDeviceDistributedOrchestratorIndex do
-  use Ecto.Migration
-
-  def change do
-    create index("devices", [:deployment_id, :updates_enabled, :updates_blocked_until])
-  end
-end
