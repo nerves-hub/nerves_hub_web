c42b45529a760352408229ee88f6d10235d5c918
Josh Kalderimis <josh.kalderimis@gmail.com>
2025-02-13 11:05:31 +1300

    Revert "Devices index WIP"
    This reverts commit f1b0215fc66d57e190e29c5d5b814f1e18eaafae.

--- priv/repo/migrations/20250211234552_create_device_distributed_orchestrator_index.exs
+++ priv/repo/migrations/20250211234552_create_device_distributed_orchestrator_index.exs
@@ -1,19 +1,7 @@
 defmodule Elixir.NervesHub.Repo.Migrations.CreateDeviceDistributedOrchestratorIndex do
   use Ecto.Migration
 
-  def up do
-    alter table(:devices) do
-      modify :updates_blocked_until, :utc_datetime, default: fragment("'-infinity'")
-    end
-
-    create index(:devices, [:deployment_id, :updates_enabled, :updates_blocked_until])
-  end
-
-  def down do
-    alter table(:devices) do
-      modify :updates_blocked_until, :utc_datetime, default: nil
-    end
-
-    drop index(:devices, [:deployment_id, :updates_enabled, :updates_blocked_until])
+  def change do
+    create index("devices", [:deployment_id, :updates_enabled, :updates_blocked_until])
   end
 end
