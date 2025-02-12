f1b0215fc66d57e190e29c5d5b814f1e18eaafae
Nate Shoemaker <transition32@gmail.com>
2025-02-12 09:12:55 -0800

    Devices index WIP

--- priv/repo/migrations/20250211234552_create_device_distributed_orchestrator_index.exs
+++ priv/repo/migrations/20250211234552_create_device_distributed_orchestrator_index.exs
@@ -1,7 +1,19 @@
 defmodule Elixir.NervesHub.Repo.Migrations.CreateDeviceDistributedOrchestratorIndex do
   use Ecto.Migration
 
-  def change do
-    create index("devices", [:deployment_id, :updates_enabled, :updates_blocked_until])
+  def up do
+    alter table(:devices) do
+      modify :updates_blocked_until, :utc_datetime, default: fragment("'-infinity'")
+    end
+
+    create index(:devices, [:deployment_id, :updates_enabled, :updates_blocked_until])
+  end
+
+  def down do
+    alter table(:devices) do
+      modify :updates_blocked_until, :utc_datetime, default: nil
+    end
+
+    drop index(:devices, [:deployment_id, :updates_enabled, :updates_blocked_until])
   end
 end
