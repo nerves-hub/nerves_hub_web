ddc200efc18d106bd9bf9514c6ae0a3f8c6ce258
Elin Olsson <elin@underjord.io>
2025-02-13 12:59:46 +0100

    make pinned devices list expandable + add connection status

--- lib/nerves_hub_web/live/orgs/index.ex
+++ lib/nerves_hub_web/live/orgs/index.ex
@@ -5,11 +5,31 @@ defmodule NervesHubWeb.Live.Orgs.Index do
   alias NervesHubWeb.Components.PinnedDevices
   alias Number.Delimit
 
+  @pinned_devices_limit 5
+
   def mount(_params, _session, %{assigns: %{user: user}} = socket) do
     socket
     |> assign(:page_title, "Organizations")
+    |> assign(:show_all_pinned?, false)
     |> assign(:pinned_devices, Devices.get_pinned_devices(user.id))
+    |> assign(:device_limit, @pinned_devices_limit)
     |> ok()
+  end
+
+  def handle_event(
+        "toggle-expand-devices",
+        _,
+        %{assigns: %{show_all_pinned?: show_all?}} = socket
+      ) do
+    socket
+    |> assign(:show_all_pinned?, !show_all?)
+    |> noreply()
+  end
+
+  defp limit_devices(devices) do
+    {limited_devices, _} = Enum.split(devices, @pinned_devices_limit)
+
+    limited_devices
   end
 
   defp format_device_count(nil), do: 0
