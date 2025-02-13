c24ee63f6da800d8091bebe26dd6fcc7264a7300
Josh Kalderimis <josh.kalderimis@gmail.com>
2025-02-13 16:12:37 +1300

    Remove some unused indexes and deprecated columns

---
+++ priv/repo/migrations/20250213030301_remove_unused_indexes_and_columns.exs
@@ -0,0 +1,22 @@
+defmodule NervesHub.Repo.Migrations.RemoveUnusedIndexesAndColumns do
+  use Ecto.Migration
+
+  def up do
+    drop(index(:devices, [:connection_status]))
+
+    alter table("device") do
+      remove(:connection_status)
+      remove(:connection_established_at)
+      remove(:connection_disconnected_at)
+      remove(:connection_last_seen_at)
+      remove(:connection_metadata)
+      remove(:connection_types)
+    end
+
+    drop(index(:device_connections, [:device_id, :established_at]))
+  end
+
+  def down do
+    raise "One way migration"
+  end
+end
