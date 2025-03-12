45ef14dd2c06444c20a6d39ea5e4594c4918b6c8
Nate Shoemaker <transition32@gmail.com>
2025-03-12 12:42:53 -0700

    Only create indexes if they don't exist

--- priv/repo/migrations/20250312132817_add_device_connection_last_seen_at_indexes.exs
+++ priv/repo/migrations/20250312132817_add_device_connection_last_seen_at_indexes.exs
@@ -4,7 +4,7 @@ defmodule NervesHub.Repo.Migrations.AddDeviceConnectionLastSeenAtIndexes do
   @disable_migration_lock true
 
   def change do
-    execute("CREATE INDEX CONCURRENTLY device_connections_not_connected_last_seen_idx ON device_connections(last_seen_at) WHERE status <> 'connected';")
-    execute("CREATE INDEX CONCURRENTLY device_connections_connected_last_seen_at_idx ON device_connections(last_seen_at) WHERE status = 'connected';")
+    execute("CREATE INDEX CONCURRENTLY IF NOT EXISTS device_connections_not_connected_last_seen_idx ON device_connections(last_seen_at) WHERE status <> 'connected';")
+    execute("CREATE INDEX CONCURRENTLY IF NOT EXISTS device_connections_connected_last_seen_at_idx ON device_connections(last_seen_at) WHERE status = 'connected';")
   end
 end
