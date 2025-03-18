b67a7a63ad8586c7f5c0fd2d8f74f88a928a5746
Nate Shoemaker <nshoes@users.noreply.github.com>
2025-03-18 12:10:12 -0700

    Migrate old Deployment audit logs to reference DeploymentGroup (#1969)
    Audit logs persist the Elixir module name of the resource it is
    auditing. In https://github.com/nerves-hub/nerves_hub_web/pull/1653, I
    renamed `Deployment` -> `DeploymentGroup` and attempting to load audit
    logs for a deployment [will blow
    up](https://github.com/nerves-hub/nerves_hub_web/blob/main/lib/nerves_hub/types.ex#L70)
    because that module doesn't exist anymore.
    
    This PR migrates old references in `audit_logs.resource_type` to
    reference the new module. Tested locally.

--- lib/nerves_hub/types.ex
+++ lib/nerves_hub/types.ex
@@ -74,8 +74,6 @@ defmodule NervesHub.Types do
         "Elixir.NervesHub.Accounts.Org",
         "Elixir.NervesHub.Accounts.User",
         "Elixir.NervesHub.ManagedDeployments.DeploymentGroup",
-        # TODO: (nshoes) remove after migrating audit_logs actor/resource type
-        "Elixir.NervesHub.Deployments.Deployment",
         "Elixir.NervesHub.Devices.Device",
         "Elixir.NervesHub.Firmwares.Firmware",
         "Elixir.NervesHub.Products.Product"
