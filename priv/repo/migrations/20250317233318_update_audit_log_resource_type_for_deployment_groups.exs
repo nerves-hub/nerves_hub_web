e1b81af95f728fa9409af2841a1af191bb44fe63
Nate Shoemaker <transition32@gmail.com>
2025-03-18 10:12:48 -0700

    Be responsible, format :)

--- priv/repo/migrations/20250317233318_update_audit_log_resource_type_for_deployment_groups.exs
+++ priv/repo/migrations/20250317233318_update_audit_log_resource_type_for_deployment_groups.exs
@@ -2,8 +2,23 @@ defmodule NervesHub.Repo.Migrations.UpdateAuditLogResourceTypeForDeploymentGroup
   use Ecto.Migration
 
   def up() do
-    execute("UPDATE audit_logs SET resource_type = 'Elixir.NervesHub.ManagedDeployments.DeploymentGroup' WHERE resource_type = 'Elixir.NervesHub.Deployments.Deployment'")
-    execute("UPDATE audit_logs SET actor_type = 'Elixir.NervesHub.ManagedDeployments.DeploymentGroup' WHERE actor_type = 'Elixir.NervesHub.Deployments.Deployment'")
+    execute("""
+      UPDATE
+        audit_logs
+      SET
+        resource_type = 'Elixir.NervesHub.ManagedDeployments.DeploymentGroup'
+      WHERE
+        resource_type = 'Elixir.NervesHub.Deployments.Deployment';
+    """)
+
+    execute("""
+      UPDATE
+        audit_logs
+      SET
+        actor_type = 'Elixir.NervesHub.ManagedDeployments.DeploymentGroup'
+      WHERE
+        actor_type = 'Elixir.NervesHub.Deployments.Deployment';
+    """)
   end
 
   def down(), do: raise "One way migration"
