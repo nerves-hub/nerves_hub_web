681856cbee162d3918125dc3761e61ccfb54b41f
Nate Shoemaker <transition32@gmail.com>
2025-03-18 10:01:00 -0700

    Use execute to bypass repo timeouts

--- priv/repo/migrations/20250317233318_update_audit_log_resource_type_for_deployment_groups.exs
+++ priv/repo/migrations/20250317233318_update_audit_log_resource_type_for_deployment_groups.exs
@@ -2,13 +2,8 @@ defmodule NervesHub.Repo.Migrations.UpdateAuditLogResourceTypeForDeploymentGroup
   use Ecto.Migration
 
   def up() do
-    repo().query!(
-      "UPDATE audit_logs SET resource_type = 'Elixir.NervesHub.ManagedDeployments.DeploymentGroup' WHERE resource_type = 'Elixir.NervesHub.Deployments.Deployment'"
-    )
-
-    repo().query!(
-      "UPDATE audit_logs SET actor_type = 'Elixir.NervesHub.ManagedDeployments.DeploymentGroup' WHERE actor_type = 'Elixir.NervesHub.Deployments.Deployment'"
-    )
+    execute("UPDATE audit_logs SET resource_type = 'Elixir.NervesHub.ManagedDeployments.DeploymentGroup' WHERE resource_type = 'Elixir.NervesHub.Deployments.Deployment'")
+    execute("UPDATE audit_logs SET actor_type = 'Elixir.NervesHub.ManagedDeployments.DeploymentGroup' WHERE actor_type = 'Elixir.NervesHub.Deployments.Deployment'")
   end
 
   def down(), do: raise "One way migration"
