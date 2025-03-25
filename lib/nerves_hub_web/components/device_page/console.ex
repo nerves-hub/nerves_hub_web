5654e880d7e95ef46e5671278cbc28b01cd554d0
Lars Wikman <lars@underjord.io>
2025-03-25 09:52:19 +0100

    Remove padding and box around the console (#1976)
    A better immersive console.

--- lib/nerves_hub_web/components/device_page/console.ex
+++ lib/nerves_hub_web/components/device_page/console.ex
@@ -31,41 +31,16 @@ defmodule NervesHubWeb.Components.DevicePage.Console do
   def render(assigns) do
     ~H"""
     <div class="size-full">
-      <div :if={authorized?(:"device:console", @org_user) && @console_active?} class="flex flex-col size-full items-start justify-between">
-        <div id="console-and-chat" class="size-full flex gap-4 p-6" phx-update="ignore">
-          <div class="flex flex-col w-full bg-zinc-900 border border-zinc-700 rounded">
-            <div class="flex justify-between items-center h-14 px-4 border-b border-zinc-700">
-              <div id="console-title" class="text-base text-neutral-50 font-medium">Console</div>
-            </div>
-            <div id="dropzone" class="grow flex p-6 gap-6">
-              <div id="console" phx-hook="Console" data-user-token={@user_token} data-device-id={@device.id} class="w-full h-full"></div>
-            </div>
+      <div class="flex flex-col size-full items-start justify-between">
+        <div id="console-and-chat" class="size-full flex bg-black p-12" phx-update="ignore" style="background-color: rgb(14, 16, 25);">
+          <div :if={authorized?(:"device:console", @org_user) && @console_active?} id="dropzone" class="grow">
+            <div id="console" phx-hook="Console" data-user-token={@user_token} data-device-id={@device.id} class="w-full h-full"></div>
           </div>
-        </div>
-      </div>
-
-      <div :if={authorized?(:"device:console", @org_user) && !@console_active?} class="flex flex-col size-full items-start justify-between">
-        <div id="console-and-chat" class="size-full flex gap-4 p-6" phx-update="ignore">
-          <div class="flex flex-col w-full bg-zinc-900 border border-zinc-700 rounded">
-            <div class="flex justify-between items-center h-14 px-4 border-b border-zinc-700">
-              <div class="text-base text-neutral-50 font-medium">Console</div>
-            </div>
-            <div class="grow flex justify-center items-center p-6 gap-6 text-medium">
-              The device console isn't currently available.
-            </div>
+          <div :if={authorized?(:"device:console", @org_user) && !@console_active?} class="grow flex justify-center items-center p-6 gap-6 text-medium font-mono">
+            The device console isn't currently available.
           </div>
-        </div>
-      </div>
-
-      <div :if={!authorized?(:"device:console", @org_user)} class="flex flex-col size-full items-start justify-between">
-        <div id="console-and-chat" class="size-full flex gap-4 p-6" phx-update="ignore">
-          <div class="flex flex-col w-full bg-zinc-900 border border-zinc-700 rounded">
-            <div class="flex justify-between items-center h-14 px-4 border-b border-zinc-700">
-              <div class="text-base text-neutral-50 font-medium">Console</div>
-            </div>
-            <div class="grow flex justify-center items-center p-6 gap-6 text-medium text-red-500">
-              You don't have the required permissions to access a Device console.
-            </div>
+          <div :if={!authorized?(:"device:console", @org_user)} class="grow flex justify-center items-center p-6 gap-6 text-medium text-red-500 font-mono">
+            You don't have the required permissions to access a Device console.
           </div>
         </div>
       </div>
