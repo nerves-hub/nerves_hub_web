b83c867befe53634ea864769b006339e9fa57db6
Nate Shoemaker <transition32@gmail.com>
2025-03-25 20:02:59 -0700

    Name indexes properly

--- priv/repo/migrations/20250326023630_index_device_firmware_metadata_architecture_and_platform.exs
+++ priv/repo/migrations/20250326023630_index_device_firmware_metadata_architecture_and_platform.exs
@@ -4,7 +4,7 @@ defmodule NervesHub.Repo.Migrations.IndexDeviceFirmwareMetadataArchitectureAndPl
   @disable_migration_lock true
 
   def change do
-    create_if_not_exists index("devices", ["(firmware_metadata #> '{platform}'::text[])"], name: :devices_firmware_platform_index, using: "BTREE", concurrently: true)
-    create_if_not_exists index("devices", ["(firmware_metadata #> '{architecture}'::text[])"], name: :devices_firmware_index, using: "BTREE", concurrently: true)
+    create_if_not_exists index("devices", ["(firmware_metadata #> '{platform}'::text[])"], name: :devices_firmware_plat_path_index, using: "BTREE", concurrently: true)
+    create_if_not_exists index("devices", ["(firmware_metadata #> '{architecture}'::text[])"], name: :devices_firmware_arch_path_index, using: "BTREE", concurrently: true)
   end
 end
