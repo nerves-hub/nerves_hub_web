78c3c9f6d7874490a45a836d8667ad5a6d83543b
Josh Kalderimis <josh.kalderimis@gmail.com>
2025-04-06 15:52:39 +1200

    Clean up the API authentication plug

--- lib/nerves_hub_web/plugs/api/user.ex
+++ lib/nerves_hub_web/plugs/api/user.ex
@@ -8,26 +8,13 @@ defmodule NervesHubWeb.API.Plugs.User do
   end
 
   def call(conn, _opts) do
-    case authenticate_user(conn) do
-      {:ok, user} ->
-        _ = mark_last_used(conn)
-
-        assign(conn, :user, user)
-
-      _error ->
-        raise NervesHubWeb.UnauthorizedError
-    end
-  end
-
-  defp authenticate_user(conn) do
-    # Prefer token authentication as the default
-    # Support legacy peer certificate auth for backwards compatibility
-    case get_req_token(conn) do
-      {:ok, token} ->
-        token_auth(token)
-
+    with {:ok, token} <- get_req_token(conn),
+         {:ok, user} <- Accounts.fetch_user_by_api_token(token),
+         :ok <- Accounts.mark_last_used(token) do
+      assign(conn, :user, user)
+    else
       _ ->
-        :error
+        raise NervesHubWeb.UnauthorizedError
     end
   end
 
@@ -36,23 +23,6 @@ defmodule NervesHubWeb.API.Plugs.User do
          [scheme, token | _] = String.split(header, " "),
          true <- String.downcase(scheme) in ["token", "bearer"] do
       {:ok, token}
-    end
-  end
-
-  defp token_auth(<<"nh", _u, "_", token::43-bytes>>) do
-    Accounts.fetch_user_by_api_token(token)
-  end
-
-  defp token_auth(_token) do
-    :forbidden
-  end
-
-  defp mark_last_used(conn) do
-    with [header] <- get_req_header(conn, "authorization"),
-         [scheme, full_token | _] = String.split(header, " "),
-         true <- String.downcase(scheme) in ["token", "bearer"],
-         <<"nh", _u, "_", token::43-bytes>> <- full_token do
-      Accounts.mark_last_used(token)
     end
   end
 end
