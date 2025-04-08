dd31a9e585222837b503fb4da86d905d369cc740
Nate Shoemaker <transition32@gmail.com>
2025-04-08 10:38:16 -0700

    Revert style changes in migration

--- priv/repo/migrations/20250204100906_create_pinned_devices.exs
+++ priv/repo/migrations/20250204100906_create_pinned_devices.exs
@@ -2,11 +2,11 @@ defmodule NervesHub.Repo.Migrations.CreatePinnedDevices do
   use Ecto.Migration
 
   def change do
-    create table(:pinned_devices) do
-      add(:user_id, references(:users, on_delete: :delete_all))
-      add(:device_id, references(:devices, on_delete: :delete_all))
+      create table(:pinned_devices) do
+        add :user_id, references(:users, on_delete: :delete_all)
+        add :device_id, references(:devices, on_delete: :delete_all)
 
-      timestamps(updated_at: false)
-    end
+        timestamps(updated_at: false)
+      end
   end
 end
