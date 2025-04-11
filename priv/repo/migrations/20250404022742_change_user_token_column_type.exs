b9d6cd0d46501bc5ce1c133b4bd0ad73a8bafeb4
Nate Shoemaker <transition32@gmail.com>
2025-04-11 16:16:26 -0700

    Update existing user_token rows with a context of 'api'

--- priv/repo/migrations/20250404022742_change_user_token_column_type.exs
+++ priv/repo/migrations/20250404022742_change_user_token_column_type.exs
@@ -1,7 +1,7 @@
 defmodule NervesHub.Repo.Migrations.ChangeUserTokenColumnType do
   use Ecto.Migration
 
-  def change do
+  def up do
     rename table(:user_tokens), :token, to: :old_token
 
     alter table(:user_tokens) do
@@ -9,6 +9,14 @@ defmodule NervesHub.Repo.Migrations.ChangeUserTokenColumnType do
       add(:context, :string, null: false, default: "api")
     end
 
+    flush()
+
+    repo().query!("UPDATE user_tokens SET context = 'api'")
+
     create(unique_index(:user_tokens, [:context, :token]))
+  end
+
+  def down do
+    raise "One way migration"
   end
 end
