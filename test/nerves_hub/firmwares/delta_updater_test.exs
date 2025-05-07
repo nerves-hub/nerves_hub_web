7e381c2751d3ef3a73dbd88dd42ecbf7154fb408
Lars Wikman <lars@underjord.io>
2025-05-07 14:50:39 +0200

    Add a delta verification test

--- test/nerves_hub/firmwares/delta_updater_test.exs
+++ test/nerves_hub/firmwares/delta_updater_test.exs
@@ -3,6 +3,12 @@ defmodule NervesHub.Firmwares.DeltaUpdateTest do
 
   alias NervesHub.Firmwares.DeltaUpdater.Default
 
+  @raw File.read!("test/fixtures/fwup/raw.conf")
+  @fat File.read!("test/fixtures/fwup/fat.conf")
+  @mixed File.read!("test/fixtures/fwup/mixed.conf")
+  @pi_style File.read!("test/fixtures/fwup/pi-style.conf")
+  @pi_style_delta File.read!("test/fixtures/fwup/pi-style-delta.conf")
+
   defp build_fw!(path, fwup_path, data_path) do
     case System.cmd("fwup", ["-c", "-f", fwup_path, "-o", path],
            stderr_to_stdout: true,
@@ -56,8 +62,10 @@ defmodule NervesHub.Firmwares.DeltaUpdateTest do
     size_a = size_a * 512
     offset_b = offset_b * 512
     size_b = size_b * 512
-    <<_::binary-size(offset_a), d1::binary-size(size_a), _::binary>> = File.read!(img_a)
-    <<_::binary-size(offset_b), d2::binary-size(size_b), _::binary>> = File.read!(img_b)
+    data_a = File.read!(img_a)
+    data_b = File.read!(img_b)
+    <<_::binary-size(offset_a), d1::binary-size(size_a), _::binary>> = data_a
+    <<_::binary-size(offset_b), d2::binary-size(size_b), _::binary>> = data_b
     compare_data?(d1, d2, 0, true)
   end
 
@@ -69,7 +77,7 @@ defmodule NervesHub.Firmwares.DeltaUpdateTest do
        ) do
     valid? =
       if chunk_1 != chunk_2 do
-        IO.puts("Difference at offset: #{offset}")
+        IO.puts("Difference at offset: #{offset} (#{trunc(offset / 512)})")
         IO.puts("chunk 1:")
         IO.puts(inspect(chunk_1, as: :binary, base: :hex))
         IO.puts("chunk 2:")
@@ -85,7 +93,7 @@ defmodule NervesHub.Firmwares.DeltaUpdateTest do
 
   defp compare_data?(<<chunk_1::binary>>, <<chunk_2::binary>>, offset, valid?) do
     if chunk_1 != chunk_2 do
-      IO.puts("Difference at offset: #{offset}")
+      IO.puts("Difference at offset: #{offset} (#{trunc(offset / 512)})")
       IO.puts("chunk 1:")
       IO.inspect(chunk_1, as: :binary, base: :hex)
       IO.puts("chunk 2:")
@@ -100,7 +108,7 @@ defmodule NervesHub.Firmwares.DeltaUpdateTest do
   @tag :tmp_dir
   test "generate valid delta for fat", %{tmp_dir: dir} do
     fwup_conf_path = Path.join(dir, "fwup.conf")
-    File.write!(fwup_conf_path, fwup_conf_fat())
+    File.write!(fwup_conf_path, @fat)
 
     data_path_1 = Path.join(dir, "data-1")
     data_1 = for _ <- 1..100, into: <<>>, do: Ecto.UUID.generate()
@@ -138,7 +146,7 @@ defmodule NervesHub.Firmwares.DeltaUpdateTest do
   @tag :tmp_dir
   test "generate valid delta for raw fwup config", %{tmp_dir: dir} do
     fwup_conf_path = Path.join(dir, "fwup.conf")
-    File.write!(fwup_conf_path, fwup_conf_raw())
+    File.write!(fwup_conf_path, @raw)
 
     data_path_1 = Path.join(dir, "data-1")
     data_1 = for _ <- 1..100, into: <<>>, do: Ecto.UUID.generate()
@@ -174,7 +182,7 @@ defmodule NervesHub.Firmwares.DeltaUpdateTest do
   @tag :tmp_dir
   test "generate valid delta for mixed fwup config", %{tmp_dir: dir} do
     fwup_conf_path = Path.join(dir, "fwup.conf")
-    File.write!(fwup_conf_path, fwup_conf_mix())
+    File.write!(fwup_conf_path, @mixed)
 
     data_path_1 = Path.join(dir, "data-1")
     data_1 = for _ <- 1..100, into: <<>>, do: Ecto.UUID.generate()
@@ -210,137 +218,55 @@ defmodule NervesHub.Firmwares.DeltaUpdateTest do
     assert compare_images?({img_b, 1024, 1024}, {img_a, 2048, 1024})
   end
 
-  defp fwup_conf_raw() do
-    """
-    define(ROOTFS_A_PART_OFFSET, 1024)
-    define(ROOTFS_A_PART_COUNT, 1024)
-    define(ROOTFS_B_PART_OFFSET, 2048)
-    define(ROOTFS_B_PART_COUNT, 1024)
+  @tag :tmp_dir
+  test "verify that a firmware is delta updatable", %{tmp_dir: dir} do
+    fwup_conf_path = Path.join(dir, "fwup.conf")
+    File.write!(fwup_conf_path, @pi_style_delta)
 
-    file-resource first {
-            host-path = "${TEST_1}"
-    }
+    data_path_1 = Path.join(dir, "data-1")
+    data_1 = for _ <- 1..100, into: <<>>, do: Ecto.UUID.generate()
+    File.write!(data_path_1, data_1)
 
-    task complete {
-        on-init {
-            raw_memset(${ROOTFS_B_PART_OFFSET}, ${ROOTFS_B_PART_COUNT}, 0)
-        }
-        on-resource first {
-          raw_write(${ROOTFS_A_PART_OFFSET})
-        }
-    }
-    task upgrade {
-        on-resource first {
-            delta-source-raw-offset=${ROOTFS_A_PART_OFFSET}
-            delta-source-raw-count=${ROOTFS_A_PART_COUNT}
-            raw_write(${ROOTFS_B_PART_OFFSET})
-        }
-    }
-    """
-  end
+    data_path_2 = Path.join(dir, "data-2")
+    data_2 = for _ <- 1..100, into: data_1, do: Ecto.UUID.generate()
+    File.write!(data_path_2, data_2)
 
-  defp fwup_conf_fat() do
-    """
-    define(BOOT_A_PART_OFFSET, 4096)
-    define(BOOT_A_PART_COUNT, 154476)
-    define-eval(BOOT_B_PART_OFFSET, "${BOOT_A_PART_OFFSET} + ${BOOT_A_PART_COUNT}")
-    define(BOOT_B_PART_COUNT, 154476)
+    fw_a = build_fw!(Path.join(dir, "a.fw"), fwup_conf_path, data_path_1)
+    fw_b = build_fw!(Path.join(dir, "b.fw"), fwup_conf_path, data_path_2)
 
-    file-resource second {
-            host-path = "${TEST_1}"
-    }
-    mbr mbr-a {
-      partition 0 {
-          block-offset = ${BOOT_A_PART_OFFSET}
-          block-count = ${BOOT_A_PART_COUNT}
-          type = 0xc # FAT32
-          boot = true
-      }
-      partition 1 {
-          block-offset = ${BOOT_B_PART_OFFSET}
-          block-count = ${BOOT_B_PART_COUNT}
-          type = 0xc # FAT32
-          boot = false
-      }
-    }
-    task complete {
-        on-init {
-            mbr_write(mbr-a)
-            fat_mkfs(${BOOT_A_PART_OFFSET}, ${BOOT_A_PART_COUNT})
-            fat_mkfs(${BOOT_B_PART_OFFSET}, ${BOOT_B_PART_COUNT})
-        }
-        on-resource second {
-          fat_write(${BOOT_A_PART_OFFSET}, "second")
-        }
-    }
-    task upgrade {
-        on-resource second {
-            delta-source-fat-offset=${BOOT_A_PART_OFFSET}
-            delta-source-fat-path="second"
-            fat_write(${BOOT_B_PART_OFFSET}, "second")
-        }
-    }
-    """
-  end
+    delta_path =
+      Default.do_delta_file(fw_a, fw_b, Path.join(dir, "delta.fw"), Path.join(dir, "work"))
 
-  defp fwup_conf_mix() do
-    """
-    define(BOOT_A_PART_OFFSET, 4096)
-    define(BOOT_A_PART_COUNT, 154476)
-    define-eval(BOOT_B_PART_OFFSET, "${BOOT_A_PART_OFFSET} + ${BOOT_A_PART_COUNT}")
-    define(BOOT_B_PART_COUNT, 154476)
-    define(ROOTFS_A_PART_OFFSET, 1024)
-    define(ROOTFS_A_PART_COUNT, 1024)
-    define(ROOTFS_B_PART_OFFSET, 2048)
-    define(ROOTFS_B_PART_COUNT, 1024)
+    %{size: b_size} = File.stat!(fw_b)
+    %{size: delta_size} = File.stat!(delta_path)
+    assert delta_size < b_size
 
-    file-resource first {
-            host-path = "${TEST_1}"
-    }
-    file-resource second {
-            host-path = "${TEST_1}"
-    }
-    mbr mbr-a {
-      partition 0 {
-          block-offset = ${BOOT_A_PART_OFFSET}
-          block-count = ${BOOT_A_PART_COUNT}
-          type = 0xc # FAT32
-          boot = true
-      }
-      partition 1 {
-          block-offset = ${BOOT_B_PART_OFFSET}
-          block-count = ${BOOT_B_PART_COUNT}
-          type = 0xc # FAT32
-          boot = false
-      }
-    }
-    task complete {
-        on-init {
-            mbr_write(mbr-a)
-            fat_mkfs(${BOOT_A_PART_OFFSET}, ${BOOT_A_PART_COUNT})
-            fat_mkfs(${BOOT_B_PART_OFFSET}, ${BOOT_B_PART_COUNT})
-            raw_memset(${ROOTFS_B_PART_OFFSET}, ${ROOTFS_B_PART_COUNT}, 0)
-        }
-        on-resource first {
-          raw_write(${ROOTFS_A_PART_OFFSET})
-        }
-        on-resource second {
-          fat_write(${BOOT_A_PART_OFFSET}, "second")
-        }
-    }
-    task upgrade {
-        on-resource first {
-            delta-source-raw-offset=${ROOTFS_A_PART_OFFSET}
-            delta-source-raw-count=${ROOTFS_A_PART_COUNT}
-            raw_write(${ROOTFS_B_PART_OFFSET})
-        }
+    img_a = complete!(fw_a, Path.join(dir, "a.img"))
+    hash_a = sha256sum(img_a)
+    img_b = complete!(fw_b, Path.join(dir, "b.img"))
+    hash_b = sha256sum(img_b)
 
-        on-resource second {
-            delta-source-fat-offset=${BOOT_A_PART_OFFSET}
-            delta-source-fat-path="second"
-            fat_write(${BOOT_B_PART_OFFSET}, "second")
-        }
-    }
-    """
+    # fs images are different
+    assert hash_a != hash_b
+
+    # Upgrade A to B
+    upgrade!(delta_path, img_a)
+    # Upgrading twice should make both slots identical
+    upgrade!(delta_path, img_a)
+
+    # Upgrade non-delta B to B, should make both slots identical
+    upgrade!(fw_b, img_b)
+    # Upgrade it again to match the slot situation/uboot-env
+    upgrade!(fw_b, img_b)
+
+    hash_a = sha256sum(img_a)
+    hash_b = sha256sum(img_b)
+    %{size: size_a} = File.stat!(img_a)
+    # -32 to skip uboot
+    fwup_size_a = trunc(size_a / 512) - 32
+    %{size: size_b} = File.stat!(img_b)
+    fwup_size_b = trunc(size_b / 512) - 32
+    assert compare_images?({img_a, 32, fwup_size_a}, {img_b, 32, fwup_size_b})
+    assert hash_a == hash_b
   end
 end
