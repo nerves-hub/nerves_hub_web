7cf599c58120ac0378e78b60c43b64f3bdcde9bc
Nate Shoemaker <transition32@gmail.com>
2025-05-08 15:40:25 -0700

    Reinstate script controller API endpoint

---
+++ test/nerves_hub_web/controllers/api/script_controller_test.exs
@@ -0,0 +1,24 @@
+defmodule NervesHubWeb.API.ScriptControllerTest do
+  use NervesHubWeb.APIConnCase, async: true
+
+  alias NervesHub.Fixtures
+
+  setup context do
+    org_key = Fixtures.org_key_fixture(context.org, context.user)
+    firmware = Fixtures.firmware_fixture(org_key, context.product)
+    device = Fixtures.device_fixture(context.org, context.product, firmware)
+
+    Map.put(context, :device, device)
+  end
+
+  describe "index" do
+    test "lists scripts", %{conn: conn, product: product, device: device, user: user} do
+      script = Fixtures.support_script_fixture(product, user)
+      conn = Map.put(conn, :assigns, %{product: product})
+      conn = get(conn, Routes.api_script_path(conn, :index, device))
+      data = [script_response] = json_response(conn, 200)["data"]
+      assert Enum.count(data) == 1
+      assert script_response["id"] == script.id
+    end
+  end
+end
