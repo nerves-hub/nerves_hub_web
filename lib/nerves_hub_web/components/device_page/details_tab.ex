nerves-hub/nerves_hub_web

commit 3c6c12d62d363c1cf672145d1bb7efb373ed5789

Device priority updates (#2103)
This work adds the concept of priority updates for devices. When
`deployment_group.queue_management == "FIFO"`, devices in a deployment
group are prioritized for updates based on the duration of their current
connection (`device.latest_connection`), with those connected the
longest being updated first. Devices marked for priority updates are
considered next (`device.priority_updates`). When
`deployment_group.queue_management = "LIFO"`, devices are prioritized by
most recently-connected (`device.first_seen_at`), and then by whether
they are marked for priority updates (`device.priority_updates`).

Devices can be marked as priority on their details page, and deployment
groups can update queue management in their settings.

lib/nerves_hub_web/components/device_page/details_tab.ex
@@ -273,19 +273,43 @@ defmodule NervesHubWeb.Components.DevicePage.DetailsTab do
             Deployment Groups
           </div>
 
-          <div class="flex pt-2 px-4 pb-6 gap-4 items-center">
-            <span class="text-sm text-nerves-gray-500">Assigned deployment group:</span>
-            <span :if={is_nil(@device.deployment_group)} class="text-sm text-nerves-gray-500">No assigned deployment group</span>
-            <.link
-              :if={@device.deployment_group}
-              navigate={~p"/org/#{@org}/#{@product}/deployment_groups/#{@device.deployment_group}"}
-              class="flex items-center gap-1 pl-1.5 pr-2.5 py-0.5 border border-zinc-700 rounded-full bg-zinc-800"
-            >
-              <svg class="w-1.5 h-1.5" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="3" cy="3" r="3" fill="#10B981" />
-              </svg>
-              <span class="text-xs text-zinc-300 tracking-tight" class="">{@device.deployment_group.name}</span>
-            </.link>
+          <div class="flex flex-col pt-2 px-4 pb-6 gap-4">
+            <div class="text-sm font-medium leading-6 text-zinc-300">
+              <form id="toggle-priority-updates" phx-change="toggle-device-priority-updates">
+                <input type="hidden" name="device[priority_updates]" value="off" />
+                <input
+                  type="checkbox"
+                  id="device[priority_updates]"
+                  name="device[priority_updates]"
+                  checked={@device.priority_updates}
+                  value="on"
+                  class="rounded border-zinc-700 text-zinc-400 focus:ring-0 checked:bg-indigo-500"
+                />
+                <label for="device[priority_updates]" class="pl-2">Priority Updates</label>
+                <div id="priority-update-info" class="inline-block align-middle relative pl-1" phx-hook="ToolTip" data-placement="right">
+                  <.icon name="info" class="stroke-zinc-400" />
+                  <div class="tooltip-content hidden w-max absolute top-0 left-0 text-xs px-2 py-1.5 rounded border border-[#3F3F46] bg-base-900 flex">
+                    When enabled, this device will be prioritized for updates and bypasses the deployment group's queue management. <br />
+                    The priority setting will automatically revert after the next successful update. <br />
+                    <div class="tooltip-arrow absolute w-2 h-2 border-[#3F3F46] bg-base-900 origin-center rotate-45"></div>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div>
+              <span class="text-sm text-nerves-gray-500">Assigned deployment group:</span>
+              <span :if={is_nil(@device.deployment_group)} class="text-sm text-nerves-gray-500">No assigned deployment group</span>
+              <.link
+                :if={@device.deployment_group}
+                navigate={~p"/org/#{@org}/#{@product}/deployment_groups/#{@device.deployment_group}"}
+                class="flex items-center gap-1 pl-1.5 pr-2.5 py-0.5 border border-zinc-700 rounded-full bg-zinc-800"
+              >
+                <svg class="w-1.5 h-1.5" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="3" cy="3" r="3" fill="#10B981" />
+                </svg>
+                <span class="text-xs text-zinc-300 tracking-tight" class="">{@device.deployment_group.name}</span>
+              </.link>
+            </div>
 
             <button
               :if={@device.deployment_group}
@@ -329,17 +353,6 @@ defmodule NervesHubWeb.Components.DevicePage.DetailsTab do
                 Add to deployment group
               </.button>
             </form>
-            <div>
-              <svg class="w-5 h-5" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M10 12.5V10M10 7.5V7.49167M17.5 10C17.5 14.1421 14.1421 17.5 10 17.5C5.85786 17.5 2.5 14.1421 2.5 10C2.5 5.85786 5.85786 2.5 10 2.5C14.1421 2.5 17.5 5.85786 17.5 10Z"
-                  stroke="#A1A1AA"
-                  stroke-width="1.2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </div>
           </div>
 
           <div :if={@update_information.update_available && @device.deployment_id} class="flex p-4 gap-4 items-center justify-between border-t border-zinc-700">
@@ -371,17 +384,6 @@ defmodule NervesHubWeb.Components.DevicePage.DetailsTab do
                 Send update
               </.button>
             </form>
-            <div>
-              <svg class="w-5 h-5" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M10 12.5V10M10 7.5V7.49167M17.5 10C17.5 14.1421 14.1421 17.5 10 17.5C5.85786 17.5 2.5 14.1421 2.5 10C2.5 5.85786 5.85786 2.5 10 2.5C14.1421 2.5 17.5 5.85786 17.5 10Z"
-                  stroke="#A1A1AA"
-                  stroke-width="1.2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </div>
           </div>
         </div>
       </div>
@@ -644,6 +646,25 @@ defmodule NervesHubWeb.Components.DevicePage.DetailsTab do
     socket
     |> assign(:support_scripts, update_script(scripts, id, %{output: nil, running?: false}))
     |> halt()
+  end
+
+  def hooked_event("toggle-device-priority-updates", _params, socket) do
+    %{assigns: %{device: device}} = socket
+
+    case Devices.update_device(device, %{priority_updates: !device.priority_updates}) do
+      {:ok, device} ->
+        socket
+        |> assign(:device, device)
+        |> put_flash(:info, "Device updated successfully.")
+        |> halt()
+
+      {:error, changeset} ->
+        Logger.info("Couldn't update device.priority_updates: #{inspect(changeset)}")
+
+        socket
+        |> put_flash(:error, "There was an issue updating the device, please check the logs.")
+        |> halt()
+    end
   end
 
   def hooked_event(_event, _params, socket), do: {:cont, socket}

lib/nerves_hub/devices.ex
@@ -668,6 +668,8 @@ defmodule NervesHub.Devices do
     |> Repo.all()
   end
 
+  @spec update_firmware_metadata(Device.t(), FirmwareMetadata.t() | nil) ::
+          {:ok, Device.t()} | {:error, Ecto.Changeset.t()}
   def update_firmware_metadata(device, nil) do
     {:ok, device}
   end
@@ -677,6 +679,8 @@ defmodule NervesHub.Devices do
     update_device(device, %{firmware_metadata: metadata})
   end
 
+  @spec update_device(Device.t(), map(), broadcast: boolean()) ::
+          {:ok, Device.t()} | {:error, Ecto.Changeset.t()}
   def update_device(%Device{} = device, params, opts \\ []) do
     changeset = Device.changeset(device, params)
 
@@ -701,8 +705,13 @@ defmodule NervesHub.Devices do
   - not be running the same firmware version associated with the deployment
   - not in the penalty box (based on `updates_blocked_until`)
 
-  The list is ordered by current connection age. Devices that have been online longer
-  are updated first.
+  If the deployment group has `enable_priority_updates` set to false (the default),
+  devices are ordered by their `latest_connection`: devices connected the longest will
+  be updated first.
+
+  If the deployment group has `enable_priority_updates` set to true,
+  devices are ordered by most recently connected for the first time (`device.first_seen_at`),
+  then by `device.priority_updates`.
   """
   @spec available_for_update(DeploymentGroup.t(), non_neg_integer()) :: [Device.t()]
   def available_for_update(deployment_group, count) do
@@ -723,7 +732,21 @@ defmodule NervesHub.Devices do
     |> where([d, firmware: f], fragment("(? #>> '{\"uuid\"}') != ?", d.firmware_metadata, f.uuid))
     |> where([inflight_update: ifu], is_nil(ifu))
     |> where([d], is_nil(d.updates_blocked_until) or d.updates_blocked_until < ^now)
-    |> order_by([latest_connection: lc], asc: lc.established_at)
+    |> then(fn query ->
+      case deployment_group.queue_management do
+        :FIFO ->
+          order_by(query, [d, latest_connection: lc],
+            desc: d.priority_updates,
+            asc: lc.established_at
+          )
+
+        :LIFO ->
+          order_by(query, [d],
+            desc: d.priority_updates,
+            desc_nulls_last: d.first_seen_at
+          )
+      end
+    end)
     |> limit(^count)
     |> Repo.all()
   end
@@ -926,6 +949,7 @@ defmodule NervesHub.Devices do
     end
   end
 
+  @spec firmware_update_successful(Device.t()) :: {:ok, Device.t()} | {:error, Changeset.t()}
   def firmware_update_successful(device) do
     :telemetry.execute([:nerves_hub, :devices, :update, :successful], %{count: 1}, %{
       identifier: device.identifier,
@@ -957,6 +981,7 @@ defmodule NervesHub.Devices do
     |> Ecto.Changeset.change()
     |> Ecto.Changeset.put_change(:update_attempts, [])
     |> Ecto.Changeset.put_change(:updates_blocked_until, nil)
+    |> Ecto.Changeset.put_change(:priority_updates, false)
     |> Repo.update()
   end
 

lib/nerves_hub/devices/device.ex
@@ -29,7 +29,8 @@ defmodule NervesHub.Devices.Device do
     :deployment_id,
     :status,
     :first_seen_at,
-    :custom_location_coordinates
+    :custom_location_coordinates,
+    :priority_updates
   ]
   @required_params [:org_id, :product_id, :identifier]
 
@@ -69,6 +70,7 @@ defmodule NervesHub.Devices.Device do
     field(:updates_enabled, :boolean, default: true)
     field(:update_attempts, {:array, :utc_datetime}, default: [])
     field(:updates_blocked_until, :utc_datetime)
+    field(:priority_updates, :boolean, default: false)
 
     field(:deleted_at, :utc_datetime)
 

lib/nerves_hub/firmwares.ex
@@ -304,8 +304,9 @@ defmodule NervesHub.Firmwares do
     {:ok, metadata}
   end
 
+  @spec metadata_from_device(map()) :: {:ok, FirmwareMetadata.t() | nil}
   def metadata_from_device(metadata) do
-    params = %{
+    metadata = %{
       uuid: Map.get(metadata, "nerves_fw_uuid"),
       architecture: Map.get(metadata, "nerves_fw_architecture"),
       platform: Map.get(metadata, "nerves_fw_platform"),
@@ -318,7 +319,25 @@ defmodule NervesHub.Firmwares do
       misc: Map.get(metadata, "nerves_fw_misc")
     }
 
-    metadata_or_firmware(params)
+    case FirmwareMetadata.changeset(%FirmwareMetadata{}, metadata).valid? do
+      true ->
+        {:ok, metadata}
+
+      false ->
+        case Map.get(metadata, :uuid) do
+          nil ->
+            {:ok, nil}
+
+          uuid ->
+            case get_firmware_by_uuid(uuid) do
+              nil ->
+                {:ok, nil}
+
+              firmware ->
+                metadata_from_firmware(firmware)
+            end
+        end
+    end
   end
 
   def create_firmware_transfer(params) do
@@ -474,29 +493,6 @@ defmodule NervesHub.Firmwares do
 
       _ ->
         params
-    end
-  end
-
-  @spec metadata_or_firmware(map()) :: {:ok, FirmwareMetadata.t() | nil}
-  def metadata_or_firmware(metadata) do
-    case FirmwareMetadata.changeset(%FirmwareMetadata{}, metadata).valid? do
-      true ->
-        {:ok, metadata}
-
-      false ->
-        case Map.get(metadata, :uuid) do
-          nil ->
-            {:ok, nil}
-
-          uuid ->
-            case get_firmware_by_uuid(uuid) do
-              nil ->
-                {:ok, nil}
-
-              firmware ->
-                metadata_from_firmware(firmware)
-            end
-        end
     end
   end
 

lib/nerves_hub/managed_deployments/deployment_group.ex
@@ -40,7 +40,8 @@ defmodule NervesHub.ManagedDeployments.DeploymentGroup do
     :penalty_timeout_minutes,
     :connecting_code,
     :total_updating_devices,
-    :current_updated_devices
+    :current_updated_devices,
+    :queue_management
   ]
 
   @derive {Phoenix.Param, key: :name}
@@ -70,6 +71,7 @@ defmodule NervesHub.ManagedDeployments.DeploymentGroup do
     field(:total_updating_devices, :integer, default: 0)
     field(:current_updated_devices, :integer, default: 0)
     field(:inflight_update_expiration_minutes, :integer, default: 60)
+    field(:queue_management, Ecto.Enum, values: [:FIFO, :LIFO], default: :FIFO)
 
     # TODO: (nshoes) this column is unused, remove after 1st March
     # field(:recalculation_type, Ecto.Enum, values: [:device, :calculator_queue], default: :device)

lib/nerves_hub_web/components/core_components.ex
@@ -366,14 +366,14 @@ defmodule NervesHubWeb.CoreComponents do
 
     ~H"""
     <div phx-feedback-for={@name}>
-      <label class="flex items-center gap-4 text-sm font-medium leading-6 text-zinc-300">
+      <span class="flex items-center gap-4 text-sm font-medium leading-6 text-zinc-300">
         <input type="hidden" name={@name} value="false" />
-        <input type="checkbox" id={@id} name={@name} value="true" checked={@checked} class="rounded border-zinc-700 text-zinc-400 focus:ring-0 checked:bg-indigo-500" {@rest} />
-        {@label}
-      </label>
-      <span :if={assigns[:hint] || assigns[:rich_hint]} class="text-xs text-zinc-400">
-        {assigns[:hint] || render_slot(assigns[:rich_hint])}
+        <input type="checkbox" id={@name} name={@name} value="true" checked={@checked} class="rounded border-zinc-700 text-zinc-400 focus:ring-0 checked:bg-indigo-500" {@rest} />
+        <label for={@name}>{@label}</label>
       </span>
+      <div :if={assigns[:hint] || assigns[:rich_hint]} class="text-xs text-zinc-400">
+        {assigns[:hint] || render_slot(assigns[:rich_hint])}
+      </div>
       <.error :for={msg <- @errors}>{msg}</.error>
     </div>
     """

lib/nerves_hub_web/components/deployment_group_page/settings.ex
@@ -35,8 +35,8 @@ defmodule NervesHubWeb.Components.DeploymentGroupPage.Settings do
             <div class="text-base text-neutral-50 font-medium">General settings</div>
           </div>
 
-          <div class="flex p-6 gap-6">
-            <div class="w-1/2 flex flex-col gap-6">
+          <div class="flex flex-col p-6 gap-6">
+            <div class="w-1/2">
               <.input field={@form[:name]} label="Name" placeholder="Production" />
             </div>
           </div>
@@ -90,6 +90,24 @@ defmodule NervesHubWeb.Components.DeploymentGroupPage.Settings do
             </div>
             <div class="w-1/2">
               <.input field={@form[:version]} value={@form[:conditions].value["version"]} label="Version requirement" placeholder="eg. 1.2.3" />
+            </div>
+          </div>
+        </div>
+
+        <div class="w-2/3 flex flex-col bg-zinc-900 border border-zinc-700 rounded">
+          <div class="flex justify-between items-center h-14 px-4 border-b border-zinc-700">
+            <div class="text-base text-neutral-50 font-medium">Device queue settings</div>
+          </div>
+
+          <div class="flex flex-col p-6 gap-6">
+            <div class="w-1/2">
+              <.input
+                field={@form[:queue_management]}
+                type="select"
+                options={[[value: "FIFO", key: "FIFO"], [value: "LIFO", key: "LIFO"]]}
+                label="Queue management"
+                hint="FIFO (First-In, First-Out) prioritizes devices that have been connected the longest for updates, while LIFO (Last-In, First-Out) prioritizes the most recently connected devices."
+              />
             </div>
           </div>
         </div>

lib/nerves_hub_web/controllers/api/device_json.ex
@@ -36,7 +36,8 @@ defmodule NervesHubWeb.API.DeviceJSON do
       org_name: device.org.name,
       product_name: device.product.name,
       # deprecated
-      last_communication: connection_last_seen_at(device)
+      last_communication: connection_last_seen_at(device),
+      priority_updates: device.priority_updates
     }
   end
 

commit 13b24e7b8b9aef9616c3ba110c006ba3d18fb89e

Fix device details deployment groups layout (#2144)
I broke this layout in #2103; I forgot to spot-check a device with a
deployment group.

lib/nerves_hub_web/components/device_page/details_tab.ex
@@ -296,7 +296,7 @@ defmodule NervesHubWeb.Components.DevicePage.DetailsTab do
                 </div>
               </form>
             </div>
-            <div>
+            <div class="flex pt-2 gap-4 items-center">
               <span class="text-sm text-nerves-gray-500">Assigned deployment group:</span>
               <span :if={is_nil(@device.deployment_group)} class="text-sm text-nerves-gray-500">No assigned deployment group</span>
               <.link
@@ -309,26 +309,25 @@ defmodule NervesHubWeb.Components.DevicePage.DetailsTab do
                 </svg>
                 <span class="text-xs text-zinc-300 tracking-tight" class="">{@device.deployment_group.name}</span>
               </.link>
+              <button
+                :if={@device.deployment_group}
+                class="p-1 border border-red-500 rounded-full bg-zinc-800"
+                data-confirm="Are you sure you want to remove the device from the deployment?"
+                aria-label="Remove device from the assigned deployment group"
+                type="button"
+                phx-click="remove-from-deployment-group"
+              >
+                <svg class="w-3 h-3" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M12.4999 5.83337H7.49992M12.4999 5.83337H14.9999M12.4999 5.83337C12.4999 4.45266 11.3806 3.33337 9.99992 3.33337C8.61921 3.33337 7.49992 4.45266 7.49992 5.83337M7.49992 5.83337H4.99992M3.33325 5.83337H4.99992M4.99992 5.83337V15C4.99992 15.9205 5.74611 16.6667 6.66659 16.6667H13.3333C14.2537 16.6667 14.9999 15.9205 14.9999 15V5.83337M14.9999 5.83337H16.6666M8.33325 9.16671V13.3334M11.6666 13.3334V9.16671"
+                    stroke="#EF4444"
+                    stroke-width="1.2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </button>
             </div>
-
-            <button
-              :if={@device.deployment_group}
-              class="p-1 border border-red-500 rounded-full bg-zinc-800"
-              data-confirm="Are you sure you want to remove the device from the deployment?"
-              aria-label="Remove device from the assigned deployment group"
-              type="button"
-              phx-click="remove-from-deployment-group"
-            >
-              <svg class="w-3 h-3" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M12.4999 5.83337H7.49992M12.4999 5.83337H14.9999M12.4999 5.83337C12.4999 4.45266 11.3806 3.33337 9.99992 3.33337C8.61921 3.33337 7.49992 4.45266 7.49992 5.83337M7.49992 5.83337H4.99992M3.33325 5.83337H4.99992M4.99992 5.83337V15C4.99992 15.9205 5.74611 16.6667 6.66659 16.6667H13.3333C14.2537 16.6667 14.9999 15.9205 14.9999 15V5.83337M14.9999 5.83337H16.6666M8.33325 9.16671V13.3334M11.6666 13.3334V9.16671"
-                  stroke="#EF4444"
-                  stroke-width="1.2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
           </div>
           <div :if={@device.status == :registered && @device.deployment_id} class="flex pt-2 px-4 pb-6 gap-4 items-center">
             <span class="text-sm text-nerves-gray-500">Please note: The device will be removed from the deployment group upon connection if the arch and platform don't match.</span>