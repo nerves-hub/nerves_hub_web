8c43c23e9dd894c141fba73870e349fed8ca62b9
Lars Wikman <lars@underjord.io>
2025-07-07 16:30:18 +0200

    Add a logout button and some docs

--- lib/nerves_hub_web/components/layouts.ex
+++ lib/nerves_hub_web/components/layouts.ex
@@ -4,5 +4,10 @@ defmodule NervesHubWeb.Layouts do
   alias NervesHubWeb.Components.Navigation
   alias Phoenix.LiveView.JS
 
+  defp toggle_user_menu(js \\ %JS{}) do
+    js
+    |> JS.toggle(to: "#user-menu")
+  end
+
   embed_templates("layouts/*")
 end
