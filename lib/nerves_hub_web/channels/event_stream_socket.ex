5699a14a0832bc0a78061ae9f38b618159b0d94f
Ky Bishop <kybishop@gmail.com>
2025-07-16 11:07:38 -0400

    improve EventStreamSocket readability

--- lib/nerves_hub_web/channels/event_stream_socket.ex
+++ lib/nerves_hub_web/channels/event_stream_socket.ex
@@ -7,24 +7,34 @@ defmodule NervesHubWeb.EventStreamSocket do
 
   @impl Phoenix.Socket
   def connect(params, socket, _connect_info) do
+    with {:ok, token} <- get_token(params),
+         {:ok, user} <- fetch_user(token) do
+      {:ok, assign(socket, :user, user)}
+    end
+  end
+
+  @impl Phoenix.Socket
+  def id(_socket), do: nil
+
+  defp get_token(params) do
     case Map.get(params, "token") do
       token when is_binary(token) and byte_size(token) > 0 ->
-        # Authenticate user and store in socket assigns
-        case Accounts.fetch_user_by_api_token(token) do
-          {:ok, user, user_token} ->
-            :ok = Accounts.mark_last_used(user_token)
-
-            {:ok, assign(socket, :user, user)}
-
-          _ ->
-            {:error, :invalid_token}
-        end
+        {:ok, token}
 
       _ ->
         {:error, :no_token}
     end
   end
 
-  @impl Phoenix.Socket
-  def id(_socket), do: nil
+  defp fetch_user(token) do
+    case Accounts.fetch_user_by_api_token(token) do
+      {:ok, user, user_token} ->
+        :ok = Accounts.mark_last_used(user_token)
+
+        {:ok, user}
+
+      _ ->
+        {:error, :invalid_token}
+    end
+  end
 end
