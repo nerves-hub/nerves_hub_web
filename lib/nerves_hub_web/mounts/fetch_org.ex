bc425be8efa218e4cfce507788a01710b65e235e
Lars Wikman <lars@underjord.io>
2026-02-17 16:54:31 +0100

    Add product banner to org/product picker

--- lib/nerves_hub_web/mounts/fetch_org.ex
+++ lib/nerves_hub_web/mounts/fetch_org.ex
@@ -1,6 +1,8 @@
 defmodule NervesHubWeb.Mounts.FetchOrg do
   import Phoenix.Component
 
+  alias NervesHub.Products
+
   def on_mount(:default, %{"org_name" => org_name}, _session, socket) do
     %{user: %{orgs: orgs}} = socket.assigns
 
@@ -8,7 +10,18 @@ defmodule NervesHubWeb.Mounts.FetchOrg do
 
     case !is_nil(org) do
       true ->
-        {:cont, assign(socket, :org, org)}
+        picker_banner_urls =
+          for product <- org.products,
+              url = Products.banner_url(product),
+              into: %{},
+              do: {product.id, url}
+
+        socket =
+          socket
+          |> assign(:org, org)
+          |> assign(:picker_banner_urls, picker_banner_urls)
+
+        {:cont, socket}
 
       false ->
         raise NervesHubWeb.NotFoundError
