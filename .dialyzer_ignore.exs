7e9af5fec9574b3b3d5d7175f7ef0606006f4e86
Pepe Márquez Romero <pepe.marquezromero@protonmail.com>
2026-02-22 09:57:43 +0100

    Fix last dialyzer warns
    Signed-off-by: Pepe Márquez Romero <pepe.marquezromero@protonmail.com>

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -5,5 +5,7 @@
   # Ecto.Multi.t() opaque type false positives — MapSet.new() returns a concrete
   # %MapSet{map: %{}} that Dialyzer can't unify with the opaque MapSet.internal(_) type,
   # causing spurious call_without_opaque warnings on Ecto.Multi pipe chains.
-  {"lib/nerves_hub/accounts.ex", :call_without_opaque}
+  {"lib/nerves_hub/accounts.ex", :call_without_opaque},
+  {"lib/nerves_hub/accounts/remove_account.ex", :call_without_opaque},
+  {"lib/nerves_hub/devices.ex", :call_without_opaque}
 ]
