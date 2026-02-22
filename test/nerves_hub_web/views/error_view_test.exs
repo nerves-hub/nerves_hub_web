3c13636dca3b321bb56eec1a98fdc4739b986087
Josh Kalderimis <josh.kalderimis@gmail.com>
2026-02-22 20:49:54 +1300

    Add support for a 400 HTTP error page (#2471)
    We see a bunch of errors in Sentry related to a missing 400 error page.
    
    Phoenix is trying to display this page because there is a bot out in the
    wild trying to see if our site is susceptible to invalid UTF8 characters
    in the URL params:
    
    `%Plug.Conn.InvalidQueryError{message: "invalid UTF-8 on urlencoded
    params, got byte 173", plug_status: 400}`
    
    This seems like a fine way to silence the silly bot.

--- test/nerves_hub_web/views/error_view_test.exs
+++ test/nerves_hub_web/views/error_view_test.exs
@@ -8,4 +8,9 @@ defmodule NervesHubWeb.ErrorViewTest do
     assert render_to_string(NervesHubWeb.ErrorView, "500.html", []) =~
              "Sorry, we tried to process your request but things didn't go so well."
   end
+
+  test "render 400.html" do
+    assert render_to_string(NervesHubWeb.ErrorView, "400.html", []) =~
+             "Sorry, we are unable to process your request as it is invalid or malformed."
+  end
 end
