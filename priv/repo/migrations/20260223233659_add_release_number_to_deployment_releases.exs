622cc58f79a2e88876c7f3a6909a9be6b46f6076
Josh Kalderimis <josh.kalderimis@gmail.com>
2026-03-02 12:48:17 +1300

    Flush migration changes before backfilling data

--- priv/repo/migrations/20260223233659_add_release_number_to_deployment_releases.exs
+++ priv/repo/migrations/20260223233659_add_release_number_to_deployment_releases.exs
@@ -3,28 +3,32 @@ defmodule NervesHub.Repo.Migrations.AddReleaseNumberToDeploymentReleases do
 
   import Ecto.Query
 
-  def up do
+  def up() do
     alter table(:deployment_releases) do
-      add :number, :integer, null: true
+      add(:number, :integer, null: true)
     end
 
-    repo().all(from(dr in "deployment_releases",
-      select: %{
-        id: dr.id,
-        deployment_group_id: dr.deployment_group_id,
-        inserted_at: dr.inserted_at,
-        num: fragment("ROW_NUMBER() OVER (PARTITION BY deployment_group_id ORDER BY inserted_at ASC)")
-      }
-    ))
+    flush()
+
+    repo().all(
+      from(dr in "deployment_releases",
+        select: %{
+          id: dr.id,
+          deployment_group_id: dr.deployment_group_id,
+          inserted_at: dr.inserted_at,
+          num: fragment("ROW_NUMBER() OVER (PARTITION BY deployment_group_id ORDER BY inserted_at ASC)")
+        }
+      )
+    )
     |> Enum.each(fn release ->
       query = from(dr in "deployment_releases", where: dr.id == ^release.id)
       {1, nil} = repo().update_all(query, set: [number: release.num])
     end)
   end
 
-  def down do
+  def down() do
     alter table(:deployment_releases) do
-      remove :number
+      remove(:number)
     end
   end
 end
