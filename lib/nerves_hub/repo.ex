752a7fe8765213a610e4d377a776f12daa60aadc
Adrian Kjær Dalhaug <adrian@enpo.no>
2026-03-26 11:38:17 +0100

    Add support for verifying self signed DB cert
    This commit reads a new environment variable DATABASE_CERT_SELF_SIGNED
    which can either be "true", "false" or unset (default "false").
    
    When the new option is enabled, the standard certificate verification
    :verify_peer is disabled (set to :verify_none), and a new verification
    function will ensure that the specified DATABASE_PEM matches the
    certificate that the database presents. This is effectively certificate
    pinning, and I've drawn inspiration from Marika Wei's implementation:
    https://mw.gl/posts/elixir_ssl/
    
    The use case is for environments where the database certificate is self
    signed and there is no option to replace it with a proper certificate
    (which is the case in some database-as-a-service offerings).

--- lib/nerves_hub/repo.ex
+++ lib/nerves_hub/repo.ex
@@ -50,4 +50,35 @@ defmodule NervesHub.Repo do
   end
 
   def destroy(struct_or_changeset), do: delete(struct_or_changeset)
+
+  # verify_fun/3 is used in config/runtime.exs to verify self signed certs
+  # that would normally return {:bad_cert, :selfsigned_peer}. This is used
+  # in environments where the database uses a self signed cert instead of a
+  # proper certificate signed by a separate CA.
+
+  # We don't verify extensions that we don't know about, as that would give :bad_cert.
+  # See https://www.erlang.org/doc/apps/ssl/ssl.html#t:common_option_cert/0 for more details.
+  def verify_fun(_, {:extension, _}, state) do
+    {:unknown, state}
+  end
+
+  # This function will in practical terms pin the certificate. If the configured cert in
+  # runtime.exs exactly matches the provided cert, then we return {:valid, :ok}.
+  # The reason for pinning is that we don't need to validate the chain, as there is
+  # no chain here - we just want to validate that the certificate is exactly the same.
+
+  # This function is only used if System.get_env("DATABASE_CERT_SELF_SIGNED") == "true",
+  # so in most setups, this function is NOT invoked.
+
+  # We do not validate the cert's date, but for self signed certs, the certificate would
+  # be reconfigured in those cases. If the admin doesn't change the cert, we assume it to
+  # still be valid.
+  def verify_fun(cert, _, state) do
+    cert_binary = :public_key.pkix_encode(:OTPCertificate, cert, :otp)
+
+    case state do
+      {:der_bin, ^cert_binary} -> {:valid, :ok}
+      _ -> {:fail, :cert_no_match}
+    end
+  end
 end
