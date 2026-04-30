9cbd35f78e1f5e7f997c50525923805502556b91
Josh Kalderimis <josh.kalderimis@gmail.com>
2026-05-01 04:35:01 +1200

    Tweak the Device export to use HTTP streaming (chunks) (#2631)
    The core change is generalising `devices_csv/1` to instead act as a
    reducer so we can pass in a custom function which allows us to stream
    the response to the user.
    
    I've also tweaked the function to select less information from the
    database, fetch more rows, and use a 90 second timeout for the
    transaction.
    
    Oh, and I added a test for the HTTP part of this.
    
    Side note: This is way more memory efficient as we aren't creating a
    500k row CSV and then sending it to the client, instead we are sending
    rows as they become available.

--- lib/nerves_hub_web/controllers/product_controller.ex
+++ lib/nerves_hub_web/controllers/product_controller.ex
@@ -2,11 +2,26 @@ defmodule NervesHubWeb.ProductController do
   use NervesHubWeb, :controller
 
   alias NervesHub.Products
+  alias NimbleCSV.RFC4180, as: CSV
+
+  @csv_header ["identifier", "description", "tags", "product", "org", "certificates"]
 
   plug(:validate_role, [org: :view] when action in [:devices_export])
 
   def devices_export(%{assigns: %{current_scope: scope}} = conn, _params) do
-    filename = "#{scope.product.name}-devices.csv"
-    send_download(conn, {:binary, Products.devices_csv(scope.product)}, filename: filename)
+    conn =
+      conn
+      |> put_resp_content_type("text/csv")
+      |> put_resp_header("content-disposition", ~s[attachment; filename="#{scope.product.name}-devices.csv"])
+      |> send_chunked(:ok)
+
+    {:ok, conn} = chunk(conn, CSV.dump_to_iodata([@csv_header]))
+
+    {:ok, conn} =
+      Products.devices_export_reducer(scope.product, conn, fn conn, line ->
+        chunk(conn, CSV.dump_to_iodata([line]))
+      end)
+
+    conn
   end
 end
