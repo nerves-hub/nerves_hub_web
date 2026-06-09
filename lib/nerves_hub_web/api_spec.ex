18134248ae62363ec1d2f8478200b415d53d812b
Josh Kalderimis <josh.kalderimis@gmail.com>
2026-06-09 16:53:46 +1200

    Correct some of the OpenAPI grouping descriptions

--- lib/nerves_hub_web/api_spec.ex
+++ lib/nerves_hub_web/api_spec.ex
@@ -71,15 +71,19 @@ defmodule NervesHubWeb.ApiSpec do
         },
         %Tag{
           name: "Deployment Groups",
-          description: "Operations related to Deployment Groups"
+          description: "Deployment Group and release management"
         },
         %Tag{
           name: "Firmwares",
           description: "Firmware uploading and management"
         },
         %Tag{
+          name: "Organizations",
+          description: "Organization management"
+        },
+        %Tag{
           name: "Organization Members",
-          description: "Organization User membership"
+          description: "Organization User membership management"
         },
         %Tag{
           name: "Products",
@@ -91,11 +95,11 @@ defmodule NervesHubWeb.ApiSpec do
         },
         %Tag{
           name: "Status",
-          description: "Application healthcheck"
+          description: "Platform healthcheck"
         },
         %Tag{
           name: "Support Scripts",
-          description: "Organization Support Script management"
+          description: "Product Support Script management"
         }
       ]
     }
